f198f8ee47d7cab8656c6352c4248838dac65345
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-04-28 20:30:34 +0200

    initial commit

---
+++ src/game/effects/effects.h
@@ -0,0 +1,10 @@
+#ifndef __EFFECTS_H__
+#define __EFFECTS_H__
+
+#include "base/effects.h"
+#include "goldrush/effects.h"
+#include "armedanddangerous/effects.h"
+#include "valleyofshadows/effects.h"
+#include "canyondiablo/effects.h"
+
+#endif
---
+++ src/game/effects/scenarios.h
@@ -0,0 +1,7 @@
+#ifndef __SCENARIOS_H__
+#define __SCENARIOS_H__
+
+#include "highnoon/scenarios.h"
+#include "fistfulofcards/scenarios.h"
+
+#endif
