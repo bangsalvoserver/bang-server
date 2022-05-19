25a6a4624e62d716807170a935369f8132e9b7a8
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-05-19 09:57:19 +0200

    gestione pulita chiusura server

--- src/net/main.cpp
+++ src/net/main.cpp
@@ -1,7 +1,10 @@
 #include <charconv>
 #include <iostream>
+#include <signal.h>
 
 #include "wsbang.h"
+
+volatile bool g_stop = false;
 
 int main(int argc, char **argv) {
     asio::io_context ctx;
@@ -20,10 +23,14 @@ int main(int argc, char **argv) {
     if (server.start(port)) {
         std::cout << "Server listening on port " << port << '\n';
 
+        ::signal(SIGTERM, [](int) {
+            g_stop = true;
+        });
+
         using frames = std::chrono::duration<int64_t, std::ratio<1, banggame::fps>>;
         auto next_frame = std::chrono::steady_clock::now() + frames{0};
 
-        while (true) {
+        while (!g_stop) {
             next_frame += frames{1};
 
             ctx.poll();
@@ -31,6 +38,8 @@ int main(int argc, char **argv) {
 
             std::this_thread::sleep_until(next_frame);
         }
+
+        std::cout << "Server stopped\n";
 
         return 0;
     } else {
