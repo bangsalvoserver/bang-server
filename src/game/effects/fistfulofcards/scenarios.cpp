8021db97fe35fdbb91a7b4541e29e1cb9962aa5e
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-05-22 18:33:41 +0200

    bugfix fistfulofcards

--- src/game/effects/fistfulofcards/scenarios.cpp
+++ src/game/effects/fistfulofcards/scenarios.cpp
@@ -160,7 +160,11 @@ namespace banggame {
         target->m_game->add_event<event_type::pre_turn_start>(target_card, [=](player *p) {
             p->m_game->add_log("LOG_RECEIVED_N_BANGS_FOR", p, target_card, p->m_hand.size());
             for (int i=0; i<p->m_hand.size(); ++i) {
-                p->m_game->queue_request<request_bang>(target_card, nullptr, p);
+                p->m_game->queue_action([=]{
+                    if (p->alive()) {
+                        p->m_game->queue_request<request_bang>(target_card, nullptr, p);
+                    }
+                });
             }
         });
     }
