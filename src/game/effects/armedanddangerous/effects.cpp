1cfbda93fc8e85021d52a06f8ddb9ea704e07898
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-06-12 18:29:50 +0200

    bugfix doppia canna

--- src/game/effects/armedanddangerous/effects.cpp
+++ src/game/effects/armedanddangerous/effects.cpp
@@ -74,7 +74,9 @@ namespace banggame {
     void effect_doublebarrel::on_play(card *origin_card, player *origin) {
         origin->m_game->add_listener<event_type::apply_bang_modifier>(origin_card, [=](player *p, request_bang *req) {
             if (p == origin) {
-                req->unavoidable = origin->get_card_sign(req->origin_card).suit == card_suit::diamonds;
+                if (origin->get_card_sign(req->origin_card).suit == card_suit::diamonds) {
+                    req->unavoidable = true;
+                }
                 origin->m_game->remove_listeners(origin_card);
             }
         });
