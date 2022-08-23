60d7b354ebb9db04e1e4e45ba7ca55c511d25e85
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-08-23 23:35:33 +0200

    flash_card for ms_abigail

--- src/game/effects/armedanddangerous/characters.cpp
+++ src/game/effects/armedanddangerous/characters.cpp
@@ -116,6 +116,7 @@ namespace banggame {
     }
 
     void effect_ms_abigail::on_play(card *origin_card, player *origin) {
+        origin->m_game->flash_card(origin_card);
         origin->m_game->pop_request();
         origin->m_game->update_request();
     }
