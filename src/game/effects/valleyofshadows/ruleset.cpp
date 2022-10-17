8764739263eb299557f3369ae82fef64bb6cb462
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-10-17 11:35:40 +0200

    fix escapable with empty hand

--- src/game/effects/valleyofshadows/ruleset.cpp
+++ src/game/effects/valleyofshadows/ruleset.cpp
@@ -6,7 +6,8 @@ namespace banggame {
     
     void ruleset_valleyofshadows::on_apply(game *game) {
         game->add_listener<event_type::apply_escapable_modifier>(nullptr, [=](card *origin_card, player *origin, const player *target, effect_flags flags, bool &value) {
-            value = value || bool(flags & effect_flags::escapable) && !ranges_contains(game->m_discards, "ESCAPE", &card::name);
+            value = value || bool(flags & effect_flags::escapable)
+                && !target->m_hand.empty() && !ranges_contains(game->m_discards, "ESCAPE", &card::name);
         });
 
         game->add_listener<event_type::check_damage_response>(nullptr, [=](bool &value) {
