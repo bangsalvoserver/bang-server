79d6d844570b5e49ff8405d559d8f0d9a5c49cd8
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-10-18 15:36:01 +0200

    remove effect_self_damage

--- src/game/effects/base/damage.cpp
+++ src/game/effects/base/damage.cpp
@@ -4,11 +4,18 @@
 #include "deathsave.h"
 
 namespace banggame {
+    
+    game_string effect_damage::verify(card *origin_card, player *origin, player *target, effect_flags flags) {
+        if (origin == target && origin->m_hp <= damage) {
+            return "ERROR_CANT_SELF_DAMAGE";
+        }
+        return {};
+    }
 
     void effect_damage::on_play(card *origin_card, player *origin, player *target, effect_flags flags) {
         if (!target->is_ghost()) {
             if (target->m_game->call_event<event_type::check_damage_response>(false)) {
-                target->m_game->queue_request_front<timer_damaging>(origin_card, origin, target, amount, flags);
+                target->m_game->queue_request_front<timer_damaging>(origin_card, origin, target, damage, flags);
             } else {
                 on_resolve(origin_card, origin, target, flags);
             }
@@ -16,24 +23,13 @@ namespace banggame {
     }
 
     void effect_damage::on_resolve(card *origin_card, player *origin, player *target, effect_flags flags) {
-        target->m_game->add_log(amount == 1 ? "LOG_TAKEN_DAMAGE" : "LOG_TAKEN_DAMAGE_PLURAL", origin_card, target, amount);
-        target->set_hp(target->m_hp - amount);
-        target->m_game->call_event<event_type::before_hit>(origin_card, origin, target, amount, flags);
+        target->m_game->add_log(damage == 1 ? "LOG_TAKEN_DAMAGE" : "LOG_TAKEN_DAMAGE_PLURAL", origin_card, target, damage);
+        target->set_hp(target->m_hp - damage);
+        target->m_game->call_event<event_type::before_hit>(origin_card, origin, target, damage, flags);
         if (target->m_hp <= 0) {
             target->m_game->queue_request<request_death>(origin_card, origin, target);
         }
-        target->m_game->call_event<event_type::after_hit>(origin_card, origin, target, amount, flags);
-    }
-    
-    game_string effect_self_damage::verify(card *origin_card, player *origin) {
-        if (origin->m_hp <= 1) {
-            return "ERROR_CANT_SELF_DAMAGE";
-        }
-        return {};
-    }
-
-    void effect_self_damage::on_play(card *origin_card, player *origin) {
-        origin->damage(origin_card, origin, 1);
+        target->m_game->call_event<event_type::after_hit>(origin_card, origin, target, damage, flags);
     }
 
     std::vector<card *> timer_damaging::get_highlights() const {
--- src/game/effects/base/damage.h
+++ src/game/effects/base/damage.h
@@ -6,16 +6,19 @@
 namespace banggame {
     
     struct effect_damage {
-        int amount;
-        effect_damage(int amount) : amount(std::max(1, amount)) {}
+        int damage;
+        effect_damage(int damage) : damage(std::max(1, damage)) {}
 
+        game_string verify(card *origin_card, player *origin, effect_flags flags = {}) {
+            return verify(origin_card, origin, origin, flags);
+        }
+        void on_play(card *origin_card, player *origin, effect_flags flags = {}) {
+            on_play(origin_card, origin, origin, flags);
+        }
+
+        game_string verify(card *origin_card, player *origin, player *target, effect_flags flags = {});
         void on_play(card *origin_card, player *origin, player *target, effect_flags flags = {});
         void on_resolve(card *origin_card, player *origin, player *target, effect_flags flags = {});
-    };
-
-    struct effect_self_damage {
-        game_string verify(card *origin_card, player *origin);
-        void on_play(card *origin_card, player *origin);
     };
 
     struct timer_damaging : timer_request, cleanup_request {
