d01dc6392ebfca5ebf168e833f7f587cea9874f1
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-11-13 16:53:23 +0100

    add snake sound

--- src/cards/valleyofshadows/snake.cpp
+++ src/cards/valleyofshadows/snake.cpp
@@ -10,6 +10,7 @@ namespace banggame {
                 target->m_game->draw_check_then(target, target_card, [=](card_sign sign) {
                     if (sign.suit == card_suit::spades) {
                         target->m_game->add_log("LOG_CARD_HAS_EFFECT", target_card);
+                        target->m_game->play_sound(nullptr, "snake");
                         target->damage(target_card, nullptr, 1);
                     }
                 });
