f466048e3df3e8a0a6e6e2f631225fd7d3dd2d26
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-11-27 12:44:03 +0100

    fix julie cutter

--- src/cards/armedanddangerous/julie_cutter.cpp
+++ src/cards/armedanddangerous/julie_cutter.cpp
@@ -9,7 +9,7 @@ namespace banggame {
         p->m_game->add_listener<event_type::after_hit>(target_card, [=](card *origin_card, player *origin, player *target, int damage, effect_flags flags) {
             if (origin && p == target && origin != target) {
                 origin->m_game->queue_action([=]{
-                    if (origin->alive()) {
+                    if (target->alive()) {
                         target->m_game->draw_check_then(target, target_card, [=](card_sign sign) {
                             if (sign.suit == card_suit::hearts || sign.suit == card_suit::diamonds) {
                                 target->m_game->add_log("LOG_CARD_HAS_EFFECT", target_card);
