1808505666da5aec2930bc15f0293b2dbe8eeeb5
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-11-27 23:57:17 +0100

    add event_type::on_player_death priorities

--- src/cards/dodgecity/greg_digger.cpp
+++ src/cards/dodgecity/greg_digger.cpp
@@ -5,7 +5,7 @@
 namespace banggame {
 
     void equip_greg_digger::on_enable(card *target_card, player *p) {
-        p->m_game->add_listener<event_type::on_player_death>(target_card, [p](player *origin, player *target) {
+        p->m_game->add_listener<event_type::on_player_death>({target_card, 1}, [p](player *origin, player *target) {
             if (p != target) {
                 p->heal(2);
             }
--- src/cards/goldrush/wanted.cpp
+++ src/cards/goldrush/wanted.cpp
@@ -5,7 +5,7 @@
 namespace banggame {
 
     void equip_wanted::on_enable(card *target_card, player *p) {
-        p->m_game->add_listener<event_type::on_player_death>(target_card, [p, target_card](player *origin, player *target) {
+        p->m_game->add_listener<event_type::on_player_death>({target_card, 3}, [p, target_card](player *origin, player *target) {
             if (origin && p == target && origin != target) {
                 origin->m_game->flash_card(target_card);
                 origin->draw_card(2, target_card);
