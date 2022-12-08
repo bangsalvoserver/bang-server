1b96e0d9cd65183582926ef380a5b1bb0dc1cb65
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-12-08 19:43:47 +0100

    optimize greygory deck

--- src/cards/wildwestshow/greygory_deck.cpp
+++ src/cards/wildwestshow/greygory_deck.cpp
@@ -5,18 +5,16 @@
 namespace banggame {
     
     void equip_greygory_deck::on_equip(card *target_card, player *target) {
-        std::vector<card *> base_characters;
-        for (card &c : target->m_game->m_context.cards) {
-            if (c.expansion == card_expansion_type{}
-                && (c.pocket == pocket_type::none
-                || (c.pocket == pocket_type::player_character && c.owner == target)))
-                base_characters.push_back(&c);
-        }
-        for (size_t i=0; i<2; ++i) {
-            size_t i2 = std::uniform_int_distribution<size_t>{0, base_characters.size() - 1}(target->m_game->rng);
-            std::swap(base_characters[i], base_characters[i2]);
-        }
-        base_characters.resize(2);
+        std::array<card *, 2> base_characters;
+        std::ranges::sample(target->m_game->m_context.cards
+            | std::views::filter([&](const card &c) {
+                return &c != target_card
+                    && c.expansion == card_expansion_type{}
+                    && (c.pocket == pocket_type::none
+                    || (c.pocket == pocket_type::player_character && c.owner == target));
+            })
+            | std::views::transform([](card &c) { return &c; }),
+            base_characters.begin(), base_characters.size(), target->m_game->rng);
 
         target->m_game->add_update<game_update_type::add_cards>(make_id_vector(base_characters), pocket_type::player_character, target);
         for (card *c : base_characters) {
