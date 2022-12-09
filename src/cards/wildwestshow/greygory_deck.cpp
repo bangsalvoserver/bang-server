48c80d99d9f726fb12aec7339c044b69ccf259a9
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-12-09 10:19:12 +0100

    use to_pointers

--- src/cards/wildwestshow/greygory_deck.cpp
+++ src/cards/wildwestshow/greygory_deck.cpp
@@ -6,14 +6,12 @@ namespace banggame {
     
     void equip_greygory_deck::on_equip(card *target_card, player *target) {
         std::array<card *, 2> base_characters;
-        std::ranges::sample(target->m_game->m_context.cards
-            | std::views::filter([&](const card &c) {
-                return &c != target_card
-                    && c.expansion == card_expansion_type{}
-                    && (c.pocket == pocket_type::none
-                    || (c.pocket == pocket_type::player_character && c.owner == target));
-            })
-            | std::views::transform([](card &c) { return &c; }),
+        std::ranges::sample(target->m_game->m_context.cards | to_pointers
+            | std::views::filter([&](card *c) {
+                return c != target_card && c->expansion == card_expansion_type{}
+                    && (c->pocket == pocket_type::none
+                    || (c->pocket == pocket_type::player_character && c->owner == target));
+            }),
             base_characters.begin(), base_characters.size(), target->m_game->rng);
 
         target->m_game->add_update<game_update_type::add_cards>(make_id_vector(base_characters), pocket_type::player_character, target);
