1279bc9886db13e4c7e9bfea65c43e55aa247066
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-12-22 17:43:21 +0100

    fix disarm

--- src/cards/canyondiablo/disarm.cpp
+++ src/cards/canyondiablo/disarm.cpp
@@ -7,10 +7,14 @@ namespace banggame {
     void effect_disarm::on_play(card *origin_card, player *origin) {
         player *shooter = origin->m_game->top_request().origin();
         effect_missed::on_play(origin_card, origin);
-        if (shooter && !shooter->empty_hand()) {
-            card *hand_card = shooter->random_hand_card();
-            origin->m_game->add_log("LOG_DISCARDED_CARD_FOR", origin_card, shooter, hand_card);
-            shooter->discard_card(hand_card);
+        if (shooter) {
+            origin->m_game->queue_action([=]{
+                if (!shooter->empty_hand()) {
+                    card *hand_card = shooter->random_hand_card();
+                    origin->m_game->add_log("LOG_DISCARDED_CARD_FOR", origin_card, shooter, hand_card);
+                    shooter->discard_card(hand_card);
+                }
+            });
         }
     }
 }
