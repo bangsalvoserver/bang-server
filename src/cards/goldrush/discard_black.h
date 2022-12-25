4f0d5a056da8a124fd38e1a41c2d76cc125e2716
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-12-25 21:45:26 +0100

    add prompt on discard_black

--- src/cards/goldrush/discard_black.h
+++ src/cards/goldrush/discard_black.h
@@ -2,10 +2,11 @@
 #define __GOLDRUSH_DISCARD_BLACK_H__
 
 #include "cards/card_effect.h"
+#include "cards/base/steal_destroy.h"
 
 namespace banggame {
 
-    struct effect_discard_black {
+    struct effect_discard_black : prompt_target_self {
         game_string verify(card *origin_card, player *origin, card *target);
         void on_play(card *origin_card, player *origin, card *target);
     };
