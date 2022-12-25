3be3289b2833062c80b6b484595c9cb5000d9780
Salvo Miosi <salvo.ilmiosi@gmail.com>
2022-12-25 21:53:58 +0100

    add prompt_target_self in squaw

--- src/cards/armedanddangerous/squaw.cpp
+++ src/cards/armedanddangerous/squaw.cpp
@@ -5,6 +5,14 @@
 
 namespace banggame {
 
+    game_string handler_squaw::on_prompt(card *origin_card, player *origin, card *target_card, opt_tagged_value<target_type::none> paid_cubes) {
+        if (target_card->owner == origin) {
+            return {"PROMPT_TARGET_SELF", origin_card};
+        } else {
+            return {};
+        }
+    }
+
     void handler_squaw::on_play(card *origin_card, player *origin, card *target_card, opt_tagged_value<target_type::none> paid_cubes) {
         const auto flags = effect_flags::escapable | effect_flags::single_target;
         if (target_card->owner && !target_card->owner->immune_to(origin_card, origin, flags)) {
--- src/cards/armedanddangerous/squaw.h
+++ src/cards/armedanddangerous/squaw.h
@@ -6,6 +6,7 @@
 namespace banggame {
 
     struct handler_squaw {
+        game_string on_prompt(card *origin_card, player *origin, card *discarded_card, opt_tagged_value<target_type::none> paid_cubes);
         void on_play(card *origin_card, player *origin, card *discarded_card, opt_tagged_value<target_type::none> paid_cubes);
     };
 }
