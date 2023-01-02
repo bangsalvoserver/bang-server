66c81607724561eb5f4ad6b045dfa2f850944333
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-01-02 21:15:31 +0100

    fix red_ringo verify

--- src/cards/armedanddangerous/red_ringo.cpp
+++ src/cards/armedanddangerous/red_ringo.cpp
@@ -10,20 +10,23 @@ namespace banggame {
 
     game_string handler_red_ringo::verify(card *origin_card, player *origin, const target_list &targets) {
         if (origin->first_character()->num_cubes < targets.size()) {
-            return {"ERROR_NOT_ENOUGH_CUBES_ON", origin->first_character()};
+            return {"ERROR_NOT_ENOUGH_CUBES_ON", origin_card};
         }
-        for (const auto &target : targets) {
-            card *target_card = target.get<target_type::card>();
-            if (target_card->num_cubes >= max_cubes) {
-                return {"ERROR_CARD_HAS_FULL_CUBES", target_card};
-            }
+        auto target_cards = targets | std::views::transform([](const play_card_target &target) -> card * {
+            return target.get<target_type::card>();
+        });
+        if (auto it = std::ranges::find_if(target_cards, [&](card *target_card) {
+            return int(std::ranges::count(target_cards, target_card)) + target_card->num_cubes > max_cubes;
+        }); it != target_cards.end()) {
+            return {"ERROR_CARD_HAS_FULL_CUBES", *it};
         }
         return {};
     }
 
     void handler_red_ringo::on_play(card *origin_card, player *origin, const target_list &targets) {
-        for (const auto &target : targets) {
-            origin->move_cubes(origin->first_character(), target.get<target_type::card>(), 1);
+        for (const play_card_target &target : targets) {
+            card *target_card = target.get<target_type::card>();
+            origin->move_cubes(origin->first_character(), target_card, 1);
         }
     }
 }
