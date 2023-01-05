77d20dfa96b368393b5d0fdff472874f046ced1e
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-01-05 12:27:45 +0100

    add role shuffling in bone_orchard

--- src/cards/wildwestshow/bone_orchard.cpp
+++ src/cards/wildwestshow/bone_orchard.cpp
@@ -8,7 +8,22 @@ namespace banggame {
         origin->m_game->add_listener<event_type::verify_revivers>(target_card, [=](player *target) {
             if (!target->alive()) {
                 target->m_game->flash_card(target_card);
-                origin->m_game->add_log("LOG_REVIVE", target, target_card);
+                target->m_game->add_log("LOG_REVIVE", target, target_card);
+
+                auto dead_players = target->m_game->m_players | ranges::views::remove_if(&player::alive);
+                if (ranges::distance(dead_players) > 1) {
+                    for (player *p : dead_players) {
+                        if (p->remove_player_flags(player_flags::role_revealed)) {
+                            target->m_game->add_update<game_update_type::player_show_role>(p, player_role::unknown);
+                        }
+                    }
+
+                    auto roles = dead_players | ranges::views::transform(&player::m_role) | ranges::to<std::vector>;
+                    std::ranges::shuffle(roles, origin->m_game->rng);
+                    for (auto [p, role] : ranges::views::zip(dead_players, roles)) {
+                        p->set_role(role);
+                    }
+                }
 
                 target->remove_player_flags(player_flags::dead);
                 target->set_hp(1);
@@ -16,8 +31,6 @@ namespace banggame {
                 for (auto *c : target->m_characters) {
                     c->on_enable(target);
                 }
-
-                // TODO set random role from dead players
             }
         });
     }
