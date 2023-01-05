f135d8c08e29176e9aef2c8dc8b6b2fb6fd60da4
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-01-05 13:12:37 +0100

    add helena zontero

--- src/cards/wildwestshow/helena_zontero.cpp
+++ src/cards/wildwestshow/helena_zontero.cpp
@@ -4,9 +4,29 @@
 
 namespace banggame {
 
-    void equip_helena_zontero::on_enable(card *target_card, player *target) {
-        // TODO
-        // draw_check_then (hearts or diamonds)
-        // shuffle all roles except sheriff
+    void equip_helena_zontero::on_enable(card *target_card, player *origin) {
+        origin->m_game->draw_check_then(nullptr, target_card, [](card_sign sign) {
+            return sign.suit == card_suit::hearts || sign.suit == card_suit::diamonds;
+        }, [=](bool condition) {
+            if (condition) {
+                auto alive_players = origin->m_game->m_players
+                    | ranges::views::filter([](player *p) {
+                        return p->alive() && p->m_role != player_role::sheriff;
+                    });
+                for (player *p : alive_players) {
+                    if (origin->m_game->m_players.size() > 3) {
+                        origin->m_game->add_update<game_update_type::player_show_role>(update_target::includes(p), p, player_role::unknown);
+                    } else {
+                        origin->m_game->add_update<game_update_type::player_show_role>(p, player_role::unknown);
+                    }
+                }
+
+                auto roles = alive_players | ranges::views::transform(&player::m_role) | ranges::to<std::vector>;
+                std::ranges::shuffle(roles, origin->m_game->rng);
+                for (auto [p, role] : ranges::views::zip(alive_players, roles)) {
+                    p->set_role(role);
+                }
+            }
+        });
     }
 }
