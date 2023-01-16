aeff3cb7991b36f324e4c37d61f51be09dae40e1
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-01-16 16:40:13 +0100

    frankie_canton: add prompt_target_self

--- src/cards/armedanddangerous/frankie_canton.h
+++ src/cards/armedanddangerous/frankie_canton.h
@@ -1,11 +1,11 @@
 #ifndef __ARMEDANDDANGEROUS_FRANKIE_CANTON_H__
 #define __ARMEDANDDANGEROUS_FRANKIE_CANTON_H__
 
-#include "cards/card_effect.h"
+#include "cards/base/steal_destroy.h"
 
 namespace banggame {
 
-    struct effect_frankie_canton {
+    struct effect_frankie_canton : prompt_target_self {
         game_string verify(card *origin_card, player *origin, card *target);
         void on_play(card *origin_card, player *origin, card *target);
     };
