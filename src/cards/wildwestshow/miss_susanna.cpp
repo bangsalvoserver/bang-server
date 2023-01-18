35cb90fb108feb927e9ba3198f7a791faa335368
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-01-18 15:40:01 +0100

    add shop cards to miss susanna

--- src/cards/wildwestshow/miss_susanna.cpp
+++ src/cards/wildwestshow/miss_susanna.cpp
@@ -6,16 +6,19 @@ namespace banggame {
 
     void equip_miss_susanna::on_enable(card *target_card, player *target) {
         target->m_game->add_listener<event_type::on_turn_end>({target_card, 1}, [=](player *origin, bool skipped) {
-            auto count = ranges::count(origin->m_played_cards
+            auto count = std::ranges::count_if(origin->m_played_cards
                 | ranges::views::transform([](const auto &pair) {
                     return ranges::views::concat(
                         ranges::views::single(pair.first),
                         pair.second
                     );
                 })
-                | ranges::views::join
-                | ranges::views::transform(&card_pocket_pair::pocket),
-                pocket_type::player_hand);
+                | ranges::views::join,
+                [](const card_pocket_pair &pair) {
+                    return pair.pocket == pocket_type::player_hand
+                        || pair.pocket == pocket_type::shop_selection
+                        || pair.pocket == pocket_type::hidden_deck && pair.origin_card->has_tag(tag_type::shopchoice);
+                });
             if ((!skipped || count) && count < 3) {
                 origin->damage(target_card, nullptr, 1);
             }
