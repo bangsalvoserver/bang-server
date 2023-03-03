33bfe2321bef024226d9c081e221f49a8798ec8e
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-03-03 12:27:09 +0100

    fix effects.h

--- src/cards/greattrainrobbery/effects.h
+++ src/cards/greattrainrobbery/effects.h
@@ -1,19 +1,24 @@
 #ifndef __GREATTRAINROBBERY_EFFECTS_H__
 #define __GREATTRAINROBBERY_EFFECTS_H__
 
+#include "benny_brawler.h"
 #include "cactus.h"
 #include "dragoon.h"
 #include "evaded.h"
 #include "evan_babbit.h"
+#include "full_steam.h"
 #include "knife_revolver.h"
+#include "manuelita.h"
 #include "map.h"
 #include "moneybag.h"
 #include "most_wanted.h"
+#include "next_stop.h"
 #include "refund.h"
 #include "ruleset.h"
 #include "shade_oconnor.h"
 #include "skip_player.h"
 #include "strongbox.h"
 #include "switch_cards.h"
+#include "traincost.h"
 
 #endif
