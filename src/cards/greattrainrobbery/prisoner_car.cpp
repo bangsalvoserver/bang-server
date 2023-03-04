9dcce75fcd1d6531afa87b8b87ed995a25ffd696
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-03-04 13:46:29 +0100

    fix prisoner_car

--- src/cards/greattrainrobbery/prisoner_car.cpp
+++ src/cards/greattrainrobbery/prisoner_car.cpp
@@ -6,7 +6,7 @@ namespace banggame {
 
     void equip_prisoner_car::on_enable(card *origin_card, player *origin) {
         origin->m_game->add_listener<event_type::apply_immunity_modifier>(origin_card, [=](card *e_origin_card, player *e_origin, const player *e_target, effect_flags flags, bool &value) {
-            if (e_origin_card && e_origin != e_target && e_target == origin && e_origin_card->name == "DUEL" || e_origin_card->name == "INDIANS") {
+            if (e_origin_card && e_origin != e_target && e_target == origin && (e_origin_card->name == "DUEL" || e_origin_card->name == "INDIANS")) {
                 origin->m_game->add_log("LOG_PLAYER_IMMUNE_TO_CARD", origin, e_origin_card, origin_card);
                 origin->m_game->flash_card(origin_card);
                 value = true;
