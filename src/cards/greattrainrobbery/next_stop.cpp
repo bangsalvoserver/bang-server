0c4786549f99928bb141b785909437038454230a
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-03-04 15:53:36 +0100

    fix effect_damage

--- src/cards/greattrainrobbery/next_stop.cpp
+++ src/cards/greattrainrobbery/next_stop.cpp
@@ -9,6 +9,6 @@ namespace banggame {
         origin->m_game->add_update<game_update_type::move_train>(origin->m_game->train_position);
         origin->m_game->queue_action([=]{
             origin->m_game->call_event<event_type::on_train_advance>(origin);
-        });
+        }, -1);
     }
 }
