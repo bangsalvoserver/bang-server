e66e07ce85656baeb996b4c9f7ccd268d1e44b91
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-03-08 15:27:47 +0100

    fix map log

--- src/cards/greattrainrobbery/map.cpp
+++ src/cards/greattrainrobbery/map.cpp
@@ -26,7 +26,7 @@ namespace banggame {
         }
 
         void on_pick(card *target_card) override {
-            target->m_game->add_log("LOG_DISCARDED_CARD_FOR", origin_card, origin, target_card);
+            target->m_game->add_log("LOG_DISCARDED_CARD_FOR", origin_card, target, target_card);
             target->m_game->move_card(target_card, pocket_type::discard_pile);
             on_resolve();
         }
