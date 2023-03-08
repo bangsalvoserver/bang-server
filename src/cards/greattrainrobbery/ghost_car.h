6d9d0584d47d634436c594d623df5deadf9df4d8
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-03-08 13:29:24 +0100

    add bot_suggestion friend to ghost car

--- src/cards/greattrainrobbery/ghost_car.h
+++ src/cards/greattrainrobbery/ghost_car.h
@@ -3,9 +3,11 @@
 
 #include "cards/card_effect.h"
 
+#include "game/bot_suggestion.h"
+
 namespace banggame {
 
-    struct equip_ghost_car {
+    struct equip_ghost_car : bot_suggestion::target_friend {
         void on_equip(card *origin_card, player *origin);
         void on_unequip(card *origin_card, player *origin);
     };
