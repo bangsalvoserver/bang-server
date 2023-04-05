ba93eb429b7d2757ca9757b43ae328674fe8d14e
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-04-05 13:04:13 +0200

    add delay and short pause to evaded

--- src/cards/greattrainrobbery/evaded.cpp
+++ src/cards/greattrainrobbery/evaded.cpp
@@ -9,10 +9,13 @@ namespace banggame {
         
         effect_missed::on_play(origin_card, origin);
 
-        if (target_card && target_card->deck == card_deck_type::main_deck && target_card->pocket != pocket_type::player_hand) {
-            origin->m_game->add_log("LOG_STOLEN_SELF_CARD", origin, target_card);
-            origin->add_to_hand(target_card);
-        }
+        origin->m_game->queue_action([=]{
+            if (target_card && target_card->deck == card_deck_type::main_deck && target_card->pocket != pocket_type::player_hand) {
+                origin->m_game->add_log("LOG_STOLEN_SELF_CARD", origin, target_card);
+                origin->m_game->add_short_pause(target_card);
+                origin->add_to_hand(target_card);
+            }
+        });
     }
 
 }
