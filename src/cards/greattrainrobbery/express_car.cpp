d112c128940f7280caf4ec09c447d61b5acc388c
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-05-09 15:27:36 +0200

    express_car : cannot repeat effect

--- src/cards/greattrainrobbery/express_car.cpp
+++ src/cards/greattrainrobbery/express_car.cpp
@@ -8,6 +8,13 @@
 
 namespace banggame {
 
+    game_string effect_express_car::get_error(card *origin_card, player *origin) {
+        if (origin->m_game->call_event<event_type::count_usages>(origin, origin_card, 0) >= 1) {
+            return {"ERROR_MAX_USAGES", origin_card, 1};
+        }
+        return {};
+    }
+
     game_string effect_express_car::on_prompt(card *origin_card, player *origin) {
         if (origin->is_bot()) {
             if (ranges::any_of(get_all_playable_cards(origin), [](card *c) { return c->pocket == pocket_type::player_hand; })) {
@@ -24,6 +31,18 @@ namespace banggame {
     }
 
     void effect_express_car::on_play(card *origin_card, player *origin) {
+        event_card_key key{origin_card, 5};
+        origin->m_game->add_listener<event_type::count_usages>(key, [=](player *e_origin, card *e_card, int &usages) {
+            if (origin_card == e_card && origin == e_origin) {
+                ++usages;
+            }
+        });
+        origin->m_game->add_listener<event_type::pre_turn_start>(key, [=](player *p) {
+            if (p != origin) {
+                origin->m_game->remove_listeners(key);
+            }
+        });
+
         ++origin->m_extra_turns;
         origin->m_game->queue_request<request_discard_hand>(origin_card, origin);
         origin->m_game->queue_action([=]{
