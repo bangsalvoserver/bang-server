1cfe0dbf473ec7ee3b58c7d78ed8ba7667cb4867
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-06-07 11:53:38 +0200

    fix fistfulofcards logs

--- src/cards/fistfulofcards/fistfulofcards.cpp
+++ src/cards/fistfulofcards/fistfulofcards.cpp
@@ -7,13 +7,15 @@ namespace banggame {
     
     void equip_fistfulofcards::on_enable(card *target_card, player *target) {
         target->m_game->add_listener<event_type::pre_turn_start>(target_card, [=](player *p) {
-            p->m_game->add_log("LOG_RECEIVED_N_BANGS_FOR", p, target_card, int(p->m_hand.size()));
-            for (int i=0; i<p->m_hand.size(); ++i) {
-                p->m_game->queue_action([=]{
-                    if (p->alive()) {
-                        p->m_game->queue_request<request_bang>(target_card, nullptr, p);
-                    }
-                }, -1);
+            if (!p->empty_hand()) {
+                p->m_game->add_log("LOG_RECEIVED_N_BANGS_FOR", p, target_card, int(p->m_hand.size()));
+                for (int i=0; i<p->m_hand.size(); ++i) {
+                    p->m_game->queue_action([=]{
+                        if (p->alive()) {
+                            p->m_game->queue_request<request_bang>(target_card, nullptr, p);
+                        }
+                    }, -1);
+                }
             }
         });
     }
