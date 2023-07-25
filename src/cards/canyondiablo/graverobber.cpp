728fde9d0a08ee51b059d1815f82b2fcdad06c12
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-07-25 20:31:59 +0200

    fix graverobber

--- src/cards/canyondiablo/graverobber.cpp
+++ src/cards/canyondiablo/graverobber.cpp
@@ -1,14 +1,18 @@
 #include "graverobber.h"
 
 #include "game/game.h"
+#include "cards/effect_context.h"
 
 namespace banggame {
 
-    void effect_graverobber::on_play(card *origin_card, player *origin, player *target) {
-        if (origin->m_game->m_discards.empty()) {
-            origin->m_game->move_card(origin->m_game->top_of_deck(), pocket_type::selection);
-        } else {
-            origin->m_game->move_card(origin->m_game->m_discards.back(), pocket_type::selection);
+    void effect_graverobber::on_play(card *origin_card, player *origin, const effect_context &ctx) {
+        int num_targets = origin->m_game->num_alive() - bool(ctx.skipped_player);
+        for (int i=0; i < num_targets; ++i) {
+            if (origin->m_game->m_discards.empty()) {
+                origin->m_game->move_card(origin->m_game->top_of_deck(), pocket_type::selection);
+            } else {
+                origin->m_game->move_card(origin->m_game->m_discards.back(), pocket_type::selection);
+            }
         }
     }
 }
--- src/cards/canyondiablo/graverobber.h
+++ src/cards/canyondiablo/graverobber.h
@@ -6,7 +6,7 @@
 namespace banggame {
 
     struct effect_graverobber {
-        void on_play(card *origin_card, player *origin, player *target);
+        void on_play(card *origin_card, player *origin, const effect_context &ctx);
     };
 }
 
