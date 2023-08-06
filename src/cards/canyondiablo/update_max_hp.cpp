4e77120f17cf28f621736dc988da0bbacd8b20ca
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-08-06 23:45:46 +0200

    fix update_max_hp ghost

--- src/cards/canyondiablo/update_max_hp.cpp
+++ src/cards/canyondiablo/update_max_hp.cpp
@@ -10,7 +10,7 @@ namespace banggame {
 
     void equip_update_max_hp::on_disable(card *target_card, player *target) {
         target->reset_max_hp();
-        if (target->m_hp > target->m_max_hp) {
+        if (target->m_hp > target->m_max_hp && !target->is_ghost()) {
             target->set_hp(target->m_max_hp);
         }
     }
