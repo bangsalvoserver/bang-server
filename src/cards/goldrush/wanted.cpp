004b8315eb0505abfcebc67970082fea2eca7655
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-09-02 15:31:28 +0200

    fix wanted

--- src/cards/goldrush/wanted.cpp
+++ src/cards/goldrush/wanted.cpp
@@ -14,7 +14,7 @@ namespace banggame {
 
     void equip_wanted::on_enable(card *target_card, player *p) {
         p->m_game->add_listener<event_type::on_player_death>({target_card, 3}, [p, target_card](player *origin, player *target) {
-            if (origin && p == target && origin != target) {
+            if (origin && origin->alive() && p == target && origin != target) {
                 origin->m_game->flash_card(target_card);
                 origin->draw_card(2, target_card);
                 origin->add_gold(1);
