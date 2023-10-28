19b7a7ad056e8767eecff255e7927cd071e94c4d
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-10-28 17:51:14 +0200

    add bot suggestion for flintlock

--- src/cards/armedanddangerous/flintlock.h
+++ src/cards/armedanddangerous/flintlock.h
@@ -2,10 +2,14 @@
 #define __ARMEDANDDANGEROUS_FLINTLOCK_H__
 
 #include "cards/card_effect.h"
+#include "game/bot_suggestion.h"
 
 namespace banggame {
 
     struct handler_flintlock {
+        bool on_check_target(card *origin_card, player *origin, player *target, bool paid_cubes) {
+            return bot_suggestion::target_enemy{}.on_check_target(origin_card, origin, target);
+        }
         void on_play(card *origin_card, player *origin, player *target, bool paid_cubes);
     };
 }
