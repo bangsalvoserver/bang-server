c76104b5be1c92e19e17d169135e9ffd774db206
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-10-30 21:21:06 +0100

    bugfix lumber_flatcar::on_disable

--- src/cards/greattrainrobbery/lumber_flatcar.cpp
+++ src/cards/greattrainrobbery/lumber_flatcar.cpp
@@ -5,10 +5,14 @@
 namespace banggame {
 
     void equip_lumber_flatcar::on_enable(card *target_card, player *target) {
-        target->m_game->add_listener<event_type::count_range_mod>(target_card, [=](const player *origin, range_mod_type type, int &value) {
+        target->m_game->add_listener<event_type::count_range_mod>({target_card, 0}, [=](const player *origin, range_mod_type type, int &value) {
             if (origin == target && type == range_mod_type::range_mod) {
                 --value;
             }
         });
+    }
+
+    void equip_lumber_flatcar::on_disable(card *target_card, player *target) {
+        target->m_game->remove_listeners(event_card_key{target_card, 0});
     }
 }
