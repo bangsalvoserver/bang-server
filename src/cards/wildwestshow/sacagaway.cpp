d81b7d4b6b6ceb37a24414a047ea2c7b908dfaea
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-11-05 13:56:17 +0100

    fix sacagaway::on_disable

--- src/cards/wildwestshow/sacagaway.cpp
+++ src/cards/wildwestshow/sacagaway.cpp
@@ -17,12 +17,12 @@ namespace banggame {
     }
 
     void equip_sacagaway::on_disable(card *target_card, player *target) {
-        target->m_game->remove_game_flags(game_flags::hands_shown);
-
         for (player *p : range_all_players(target)) {
             for (card *c : p->m_hand) {
                 target->m_game->set_card_visibility(c, p);
             }
         }
+
+        target->m_game->remove_game_flags(game_flags::hands_shown);
     }
 }
