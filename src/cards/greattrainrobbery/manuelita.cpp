025c592621294fbe4188176b0769c59cf67a955a
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-11-20 09:31:02 +0100

    fix on_train_advance request priorities

--- src/cards/greattrainrobbery/manuelita.cpp
+++ src/cards/greattrainrobbery/manuelita.cpp
@@ -9,8 +9,10 @@ namespace banggame {
     void equip_manuelita::on_enable(card *target_card, player *origin) {
         origin->m_game->add_listener<event_type::on_train_advance>({target_card, 2}, [=](player *target, shared_effect_context ctx) {
             if (origin->m_game->train_position == origin->m_game->m_stations.size()) {
-                origin->m_game->flash_card(target_card);
-                origin->draw_card(2, target_card);
+                origin->m_game->queue_action([=]{
+                    origin->m_game->flash_card(target_card);
+                    origin->draw_card(2, target_card);
+                }, -1);
             }
         });
     }
--- src/cards/greattrainrobbery/shade_oconnor.cpp
+++ src/cards/greattrainrobbery/shade_oconnor.cpp
@@ -8,7 +8,7 @@ namespace banggame {
     
     struct request_shade_oconnor : request_auto_select {
         request_shade_oconnor(card *origin_card, player *target)
-            : request_auto_select(origin_card, nullptr, target) {}
+            : request_auto_select(origin_card, nullptr, target, {}, -1) {}
         
         game_string status_text(player *owner) const override {
             if (target == owner) {
