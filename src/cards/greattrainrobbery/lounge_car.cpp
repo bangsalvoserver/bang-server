5827de4810c0af987912f96e33630e7ef6f3d4a5
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-11-22 20:31:27 +0100

    add target_set for lounge_car

--- src/cards/greattrainrobbery/lounge_car.cpp
+++ src/cards/greattrainrobbery/lounge_car.cpp
@@ -28,6 +28,19 @@ namespace banggame {
             }
         }
 
+        target_list get_target_set() const override {
+            return target->m_game->m_players
+                | ranges::views::filter([&](player *p) {
+                    return ranges::any_of(target->m_game->m_selection, [&](card *target_card) {
+                        return !filters::check_player_filter(target, target_card->equip_target, p);
+                    });
+                })
+                | ranges::views::transform([](player *p) {
+                    return play_card_target{enums::enum_tag<target_type::player>, p};
+                })
+                | ranges::to<target_list>;
+        }
+
         game_string status_text(player *owner) const override {
             if (owner == target) {
                 return {"STATUS_LOUNGE_CAR", origin_card};
