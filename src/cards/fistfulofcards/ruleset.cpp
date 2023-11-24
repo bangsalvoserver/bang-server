bf5eebecc8696534a8bc725bdbf071ce0da59a99
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-11-24 23:00:06 +0100

    remove queue_action in draw_scenario_card

--- src/cards/fistfulofcards/ruleset.cpp
+++ src/cards/fistfulofcards/ruleset.cpp
@@ -5,14 +5,12 @@
 namespace banggame {
 
     void ruleset_fistfulofcards::on_apply(game *game) {
-        game->add_listener<event_type::on_turn_switch>({nullptr, 2}, [](player *origin) {
-            if (origin == origin->m_game->m_first_player && !origin->m_game->m_scenario_deck.empty()) {
-                if (bool(origin->m_game->m_scenario_deck.back()->expansion & expansion_type::fistfulofcards)) {
-                    origin->m_game->queue_action([=]{
-                        origin->m_game->draw_scenario_card();
-                    });
+        if (!bool(game->m_options.expansions & expansion_type::highnoon)) {
+            game->add_listener<event_type::on_turn_switch>({nullptr, 2}, [](player *origin) {
+                if (origin == origin->m_game->m_first_player && !origin->m_game->m_scenario_deck.empty()) {
+                    origin->m_game->draw_scenario_card();
                 }
-            }
-        });
+            });
+        }
     }
 }
--- src/cards/highnoon/ruleset.cpp
+++ src/cards/highnoon/ruleset.cpp
@@ -7,11 +7,7 @@ namespace banggame {
     void ruleset_highnoon::on_apply(game *game) {
         game->add_listener<event_type::on_turn_switch>({nullptr, 2}, [](player *origin) {
             if (origin == origin->m_game->m_first_player && !origin->m_game->m_scenario_deck.empty()) {
-                if (bool(origin->m_game->m_scenario_deck.back()->expansion & expansion_type::highnoon)) {
-                    origin->m_game->queue_action([=]{
-                        origin->m_game->draw_scenario_card();
-                    });
-                }
+                origin->m_game->draw_scenario_card();
             }
         });
     }
