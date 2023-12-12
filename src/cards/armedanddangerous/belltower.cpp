1d34ebbfa63864514c4988dcd0d87ef446ca4de9
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-12-12 16:42:15 +0100

    fix belltower + fanning

--- src/cards/armedanddangerous/belltower.cpp
+++ src/cards/armedanddangerous/belltower.cpp
@@ -9,7 +9,8 @@ namespace banggame {
     game_string modifier_belltower::get_error(card *origin_card, player *origin, card *playing_card) {
         if (std::ranges::none_of(playing_card->get_effect_list(origin->m_game->pending_requests()),
             [](const effect_holder &holder) {
-                return bool(holder.player_filter & (target_player_filter::reachable | target_player_filter::range_1 | target_player_filter::range_2));
+                return bool(holder.player_filter & (target_player_filter::reachable | target_player_filter::range_1 | target_player_filter::range_2))
+                    || holder.target == target_type::adjacent_players;
             })
         ) {
             return {"ERROR_NO_RANGED_TARGET", origin_card, playing_card};
