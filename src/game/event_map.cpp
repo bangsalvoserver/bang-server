b1ab97b4b16f753f9244e7d7621d307097aa3349
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-12-14 08:48:01 +0100

    optimize event_listener, remove one layer of indirection

--- src/game/event_map.cpp
+++ src/game/event_map.cpp
@@ -2,35 +2,34 @@
 
 namespace banggame {
     
-    void listener_map::do_add_listener(event_card_key key, listener_ptr &&ptr) {
-        auto it = m_listeners.emplace(std::move(ptr));
-        m_map.emplace(key, it);
+    void listener_map::do_add_listener(event_card_key key, size_t id, event_listener_ptr &&ptr) {
+        auto listener = m_listeners.emplace(id, std::move(ptr), key.priority);
+        m_map.emplace(key, listener);
     }
 
     void listener_map::do_remove_listeners(iterator_map_range range) {
         if (range.empty()) return;
 
-        for (const auto &[key, it] : range) {
-            auto &listener = **it;
-            if (listener.active) {
+        for (auto &[key, listener] : range) {
+            if (listener->active) {
                 if (m_lock) {
-                    m_to_remove.push_back(it);
-                    listener.active = false;
+                    m_to_remove.push_back(listener);
+                    listener->active = false;
                 } else {
-                    m_listeners.erase(it);
+                    m_listeners.erase(listener);
                 }
             }
         }
         m_map.erase(range.begin(), range.end());
     }
 
-    void listener_map::do_call_event(size_t id, detail::event_listener_invoke_fun fun, const void *tuple) {
+    void listener_map::do_call_event(size_t id, event_listener_invoke_fun fun, const void *tuple) {
         auto [low, high] = m_listeners.equal_range(id);
-        if (low == high) return;
+        std::ranges::subrange range(low, high);
+        if (range.empty()) return;
 
         ++m_lock;
-        for (auto it = low; it != high; ++it) {
-            const auto &listener = **it;
+        for (const event_listener &listener : range) {
             if (listener.id == id && listener.active) {
                 std::invoke(fun, listener, tuple);
             }
@@ -38,8 +37,8 @@ namespace banggame {
         --m_lock;
         
         if (!m_lock && !m_to_remove.empty()) {
-            for (auto it : m_to_remove) {
-                m_listeners.erase(it);
+            for (listener_iterator listener : m_to_remove) {
+                m_listeners.erase(listener);
             }
             m_to_remove.clear();
         }
--- src/game/event_map.h
+++ src/game/event_map.h
@@ -7,24 +7,58 @@
 
 #include <functional>
 #include <memory>
-#include <unordered_set>
+#include <vector>
 #include <set>
 #include <map>
 
-namespace banggame::detail {
+namespace banggame {
 
     struct event_listener_base {
-        const size_t id;
-        const int priority;
-        bool active = true;
-
-        event_listener_base(size_t id, int priority)
-            : id{id}
-            , priority{priority} {}
-
         virtual ~event_listener_base() = default;
+    };
 
-        auto operator <=> (const event_listener_base &other) const {
+    using event_listener_ptr = std::unique_ptr<event_listener_base>;
+
+    template<typename T>
+    concept event = reflector::reflectable<T>;
+
+    template<event T>
+    struct event_listener_interface : event_listener_base {
+        static size_t get_id() {
+            return reinterpret_cast<size_t>(&event_listener_interface<T>::get_id);
+        }
+
+        virtual void operator()(const reflector::as_ref_tuple_t<T> &tuple) const = 0;
+    };
+
+    template<typename Function, typename T>
+    concept applicable = requires (Function fun, T tup) {
+        std::apply(fun, tup);
+    };
+
+    template<typename Function, typename T>
+    concept applicable_to_event = event<T> && applicable<Function, reflector::as_ref_tuple_t<T>>;
+
+    template<typename T, typename Function> requires applicable_to_event<Function, T>
+    class event_listener_impl : public event_listener_interface<T>, private Function {
+    public:
+        template<std::convertible_to<Function> U>
+        event_listener_impl(U &&function)
+            : Function(std::forward<U>(function)) {}
+
+        void operator()(const reflector::as_ref_tuple_t<T> &tuple) const override {
+            std::apply(static_cast<Function>(*this), tuple);
+        }
+    };
+
+    struct event_listener {
+        size_t id;
+        event_listener_ptr ptr;
+        int priority;
+        
+        mutable bool active = true;
+
+        auto operator <=> (const event_listener &other) const {
             if (id == other.id) {
                 return other.priority <=> priority;
             } else {
@@ -36,101 +70,17 @@ namespace banggame::detail {
             return id <=> other_id;
         }
 
-        template<typename T>
-        void invoke(const void *tuple) const;
-    };
-
-    using event_listener_invoke_fun = void (event_listener_base:: *)(const void *) const;
-
-    template<reflector::reflectable T, size_t ... Is>
-    auto build_tuple_helper(const T &value, std::index_sequence<Is...>) {
-        return std::tie(reflector::get_field_data<Is, T>(value).get() ...);
-    }
-
-    template<reflector::reflectable T>
-    auto build_tuple(const T &value) {
-        return build_tuple_helper(value, std::make_index_sequence<reflector::num_fields<T>>());
-    }
-
-    template<typename T>
-    using build_tuple_t = decltype(build_tuple(std::declval<const T &>()));
-
-    template<typename T>
-    struct event_listener : event_listener_base {
-        static size_t get_id() {
-            return reinterpret_cast<size_t>(&event_listener<T>::get_id);
-        }
-
-        event_listener(size_t priority): event_listener_base(get_id(), priority) {}
-
-        virtual void operator()(const build_tuple_t<T> &tuple) const = 0;
-    };
-
-    template<typename T>
-    void event_listener_base::invoke(const void *tuple) const {
-        std::invoke(static_cast<const event_listener<T> &>(*this), *static_cast<const build_tuple_t<T> *>(tuple));
-    }
-
-    template<typename Function, typename Tuple>
-    concept applicable = requires (Function fun, Tuple tup) {
-        std::apply(fun, tup);
-    };
-
-    template<typename T, typename Function> requires applicable<Function, build_tuple_t<T>>
-    class event_listener_impl : public event_listener<T>, private Function {
-    public:
-        template<std::convertible_to<Function> U>
-        event_listener_impl(size_t priority, U &&function)
-            : event_listener<T>(priority)
-            , Function(std::forward<U>(function)) {}
-
-        void operator()(const build_tuple_t<T> &tuple) const override {
-            std::apply(static_cast<Function>(*this), tuple);
+        template<event T>
+        void invoke(const void *tuple) const {
+            std::invoke(static_cast<const event_listener_interface<T> &>(*ptr), *static_cast<const reflector::as_ref_tuple_t<T> *>(tuple));
         }
     };
 
-    template<typename T>
-    concept dereferenceable = requires(T ptr) {
-        *ptr;
-    };
-
-    template<dereferenceable T> using deref_t = std::decay_t<decltype(*std::declval<const T &>())>;
-
-    template<typename T, typename U>
-    concept comparable = requires (T lhs, U rhs) {
-        lhs < rhs;
-    };
-
-    struct deref_less {
-        struct is_transparent {};
-
-        template<dereferenceable T>
-        bool operator()(const T &lhs, const T &rhs) const {
-            return *lhs < *rhs;
-        }
-
-        template<dereferenceable T, typename U> requires comparable<deref_t<T>, U>
-        bool operator()(const T &lhs, const U &rhs) const {
-            return *lhs < rhs;
-        }
-
-        template<dereferenceable T, typename U> requires comparable<deref_t<T>, U>
-        bool operator()(const U &lhs, const T &rhs) const {
-            return lhs < *rhs;
-        }
-    };
-
-}
-
-namespace banggame {
-
-    template<typename T>
-    constexpr size_t event_listener_buffer_size = 16;
+    using event_listener_invoke_fun = void (event_listener:: *)(const void *) const;
 
     class listener_map {
     private:
-        using listener_ptr = std::unique_ptr<detail::event_listener_base>;
-        using listener_set = std::multiset<listener_ptr, detail::deref_less>;
+        using listener_set = std::multiset<event_listener, std::less<>>;
         using listener_iterator = listener_set::const_iterator;
 
         using iterator_map = std::multimap<event_card_key, listener_iterator, std::less<>>;
@@ -145,14 +95,15 @@ namespace banggame {
         int m_lock = 0;
 
     private:
-        void do_add_listener(event_card_key key, listener_ptr &&ptr);
+        void do_add_listener(event_card_key key, size_t id, event_listener_ptr &&ptr);
         void do_remove_listeners(iterator_map_range range);
-        void do_call_event(size_t id, detail::event_listener_invoke_fun fun, const void *tuple);
+        void do_call_event(size_t id, event_listener_invoke_fun fun, const void *tuple);
 
     public:
-        template<typename T, typename Function> requires detail::applicable<Function, detail::build_tuple_t<T>>
+        template<typename T, typename Function> requires applicable_to_event<Function, T>
         void add_listener(event_card_key key, Function &&fun) {
-            do_add_listener(key, std::make_unique<detail::event_listener_impl<T, std::decay_t<Function>>>(key.priority, std::forward<Function>(fun)));
+            do_add_listener(key, event_listener_interface<T>::get_id(),
+                std::make_unique<event_listener_impl<T, std::decay_t<Function>>>(std::forward<Function>(fun)));
         }
 
         void remove_listeners(auto key) {
@@ -160,10 +111,10 @@ namespace banggame {
             do_remove_listeners({low, high});
         }
 
-        template<typename T>
+        template<event T>
         void call_event(const T &value) {
-            auto tuple = detail::build_tuple(value);
-            do_call_event(detail::event_listener<T>::get_id(), &detail::event_listener_base::invoke<T>, &tuple);
+            auto tuple = reflector::as_ref_tuple(value);
+            do_call_event(event_listener_interface<T>::get_id(), &event_listener::invoke<T>, &tuple);
         }
     };
 
