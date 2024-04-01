0ee7ad64a473f9f49c5cbd73d9fb424614a581e1
Salvo Miosi <salvo.ilmiosi@gmail.com>
2024-04-01 14:45:32 +0200

    add prompt_wasteful_heal for beer

--- src/effects/base/beer.cpp
+++ src/effects/base/beer.cpp
@@ -3,19 +3,27 @@
 #include "game/game.h"
 
 namespace banggame {
+
+    static int get_beer_heal_amount(player *target) {
+        int amount = 1;
+        target->m_game->call_event(event_type::apply_beer_modifier{ target, amount });
+        return amount;
+    }
     
     game_string effect_beer::on_prompt(card *origin_card, player *target) {
         if (target->m_game->num_alive() <= 2 || (target->m_hp == target->m_max_hp)) {
             return {"PROMPT_CARD_NO_EFFECT", origin_card};
+        }
+        int amount = get_beer_heal_amount(target);
+        if (target->m_hp + amount > target->m_max_hp) {
+            return {"PROMPT_WASTEFUL_HEAL", origin_card, amount, target->m_hp + amount - target->m_max_hp};
         }
         return {};
     }
 
     void effect_beer::on_play(card *origin_card, player *target) {
         if (target->m_game->num_alive() > 2) {
-            int amount = 1;
-            target->m_game->call_event(event_type::apply_beer_modifier{ target, amount });
-            target->heal(amount);
+            target->heal(get_beer_heal_amount(target));
         }
         target->m_game->call_event(event_type::on_play_beer{ target });
     }
