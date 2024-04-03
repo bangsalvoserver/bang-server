04c0e460968e1b9ecbd66cf1f73a580af774cc8f
Salvo Miosi <salvo.ilmiosi@gmail.com>
2024-04-03 10:59:29 +0200

    cleanup flintlock

--- src/effects/armedanddangerous/flintlock.cpp
+++ src/effects/armedanddangerous/flintlock.cpp
@@ -2,16 +2,12 @@
 
 #include "effects/base/bang.h"
 
-#include "cards/game_enums.h"
-
 #include "game/game.h"
 #include "game/filters.h"
 
 namespace banggame {
 
-    void effect_flintlock::on_play(card *origin_card, player *origin, player *target, const effect_context &ctx) {
-        origin->m_game->add_log("LOG_PLAYED_CARD_ON", origin_card, origin, target);
-        auto req = std::make_shared<request_bang>(origin_card, origin, target, effect_flags::escapable | effect_flags::single_target);
+    void effect_flintlock::on_play(card *origin_card, player *origin, const effect_context &ctx) {
         if (!filters::get_selected_cubes(origin_card, ctx).empty()) {
             origin->m_game->add_listener<event_type::on_missed>(origin_card, [=](card *origin_card, player *p, player *target, card *missed_card, effect_flags flags) {
                 if (origin == p) {
@@ -27,6 +23,5 @@ namespace banggame {
                 origin->m_game->remove_listeners(origin_card);
             }, 90);
         }
-        origin->m_game->queue_request(std::move(req));
     }
 }
--- src/effects/armedanddangerous/flintlock.h
+++ src/effects/armedanddangerous/flintlock.h
@@ -2,12 +2,11 @@
 #define __ARMEDANDDANGEROUS_FLINTLOCK_H__
 
 #include "cards/card_effect.h"
-#include "game/bot_suggestion.h"
 
 namespace banggame {
 
-    struct effect_flintlock : bot_suggestion::target_enemy {
-        void on_play(card *origin_card, player *origin, player *target, const effect_context &ctx);
+    struct effect_flintlock {
+        void on_play(card *origin_card, player *origin, const effect_context &ctx);
     };
 
     DEFINE_EFFECT(flintlock, effect_flintlock)
