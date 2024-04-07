0d00f7216d955baad92df0bf33fddbae11f653e6
Salvo Miosi <salvo.ilmiosi@gmail.com>
2024-04-07 16:41:37 +0200

    fix duck prompt

--- src/effects/armedanddangerous/duck.cpp
+++ src/effects/armedanddangerous/duck.cpp
@@ -6,16 +6,16 @@
 
 namespace banggame {
 
-    static int get_bang_strength(player *origin) {
+    static int get_bang_damage(player *origin) {
         if (auto req = origin->m_game->top_request<request_bang>(origin)) {
-            return req->bang_strength;
+            return req->bang_damage;
         }
         return 1;
     }
 
     game_string effect_duck::on_prompt(card *origin_card, player *origin, const effect_context &ctx) {
         if (filters::get_selected_cubes(origin_card, ctx).empty()) {
-            if (!origin->is_bot() || origin->m_hp > get_bang_strength(origin)) {
+            if (!origin->is_bot() || origin->m_hp > get_bang_damage(origin)) {
                 return {"PROMPT_NO_REDRAW", origin_card};
             }
         }
