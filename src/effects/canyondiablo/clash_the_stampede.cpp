1b1cb1a740706c27be13d6521834f78a60e445f3
Salvo Miosi <salvo.ilmiosi@gmail.com>
2024-04-09 10:08:01 +0200

    fix clash_the_stampede

--- src/effects/canyondiablo/clash_the_stampede.cpp
+++ src/effects/canyondiablo/clash_the_stampede.cpp
@@ -3,22 +3,69 @@
 #include "effects/wildwestshow/youl_grinner.h"
 
 #include "game/game.h"
+#include "game/possible_to_play.h"
 
 namespace banggame {
+
+    static int get_max_num_cards(player *target) {
+        return rn::max(target->m_game->m_players
+            | rv::transform([](player *p) {
+                return static_cast<int>(p->m_hand.size());
+            }));
+    }
+
+    struct request_clash_the_stampede : request_base, interface_target_set {
+        request_clash_the_stampede(card *origin_card, player *target)
+            : request_base(origin_card, nullptr, target)
+            , num_cards{get_max_num_cards(target)} {}
+        
+        int num_cards;
+
+        bool in_target_set(const player *target_player) const override {
+            return target_player != target && target_player->m_hand.size() == num_cards;
+        }
+
+        void on_update() override {
+            auto target_set = target->m_game->m_players
+                | rv::filter([&](const player *p) { return in_target_set(p); });
+            if (num_cards == 0 || rn::empty(target_set)) {
+                target->m_game->pop_request();
+            } else if (player *target_player = get_single_element(target_set)) {
+                on_pick_player(target_player);
+            }
+        }
+
+        void on_pick_player(player *target_player) {
+            target->m_game->pop_request();
+            target->m_game->queue_request<request_youl_grinner>(origin_card, target, target_player);
+        }
+
+        game_string status_text(player *owner) const override {
+            if (owner == target) {
+                return {"STATUS_CLASH_THE_STAMPEDE", origin_card};
+            } else {
+                return {"STATUS_CLASH_THE_STAMPEDE_OTHER", target, origin_card};
+            }
+        }
+    };
 
     void equip_clash_the_stampede::on_enable(card *target_card, player *target) {
         target->m_game->add_listener<event_type::on_turn_start>(target_card, [=](player *origin) {
             if (target == origin) {
-                auto max_ncards = rn::max(target->m_game->m_players
-                    | rv::transform([](player *p) { return p->m_hand.size(); }));
-                if (max_ncards > 0) {
-                    for (player *p : range_other_players(target)) {
-                        if (p->m_hand.size() == max_ncards) {
-                            target->m_game->queue_request<request_youl_grinner>(target_card, target, p);
-                        }
-                    }
-                }
+                target->m_game->queue_request<request_clash_the_stampede>(target_card, target);
             }
         });
+    }
+
+    game_string effect_clash_the_stampede::get_error(card *origin_card, player *origin, player *target) {
+        if (origin->m_game->top_request<request_clash_the_stampede>(origin) == nullptr) {
+            return "ERROR_INVALID_ACTION";
+        }
+        return {};
+    }
+
+    void effect_clash_the_stampede::on_play(card *origin_card, player *origin, player *target) {
+        auto req = origin->m_game->top_request<request_clash_the_stampede>();
+        req->on_pick_player(target);
     }
 }
--- src/effects/canyondiablo/clash_the_stampede.h
+++ src/effects/canyondiablo/clash_the_stampede.h
@@ -10,6 +10,13 @@ namespace banggame {
     };
 
     DEFINE_EQUIP(clash_the_stampede, equip_clash_the_stampede)
+
+    struct effect_clash_the_stampede {
+        game_string get_error(card *origin_card, player *origin, player *target);
+        void on_play(card *origin_card, player *origin, player *target);
+    };
+
+    DEFINE_EFFECT(clash_the_stampede, effect_clash_the_stampede)
 }
 
 #endif
