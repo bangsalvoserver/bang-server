4c33396330196fb4daa224598df13fb8af9ca540
Salvo Miosi <salvo.ilmiosi@gmail.com>
2024-07-14 00:26:14 +0200

    fix rulesets

--- src/effects/rulesets.h
+++ src/effects/rulesets.h
@@ -9,5 +9,27 @@
 #include "greattrainrobbery/ruleset.h"
 #include "highnoon/ruleset.h"
 #include "valleyofshadows/ruleset.h"
+    
+namespace banggame {
+
+    template<expansion_type E>
+    constexpr const ruleset_vtable *get_ruleset_vtable() {
+        if constexpr ( requires { ruleset_vtable_map<E>::value; }) {
+            return &ruleset_vtable_map<E>::value;
+        } else {
+            return nullptr;
+        }
+    }
+
+    inline const ruleset_vtable *get_ruleset_vtable(expansion_type expansion) {
+        static constexpr const auto &values = enums::enum_values<expansion_type>();
+        static constexpr auto lookup = []<size_t ... Is>(std::index_sequence<Is...>) {
+            return std::array { get_ruleset_vtable<values[Is]>() ... };
+        }(std::make_index_sequence<values.size()>());
+        
+        return lookup[enums::indexof(expansion)];
+    }
+    
+}
 
 #endif
