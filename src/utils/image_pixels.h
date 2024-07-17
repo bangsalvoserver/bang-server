570863d0ebc3e4a25f1983e632ee72f8618b9e40
Salvo Miosi <salvo.ilmiosi@gmail.com>
2024-07-17 16:49:57 +0200

    fixup image_pixels serializers

--- src/utils/image_pixels.h
+++ src/utils/image_pixels.h
@@ -2,7 +2,7 @@
 #define __IMAGE_PIXELS_H__
 
 #include "utils/base64.h"
-#include "utils/json_serial.h"
+#include "utils/json_aggregate.h"
 
 namespace utils {
     struct image_pixels {
@@ -13,35 +13,42 @@ namespace utils {
 }
 
 namespace json {
-    template<typename Context>
-    struct serializer<utils::image_pixels, Context> {
-        json operator()(const utils::image_pixels &value) const {
-            return {
-                {"width", value.width},
-                {"height", value.height},
-                {"pixels", base64::base64_encode(value.pixels)}
-            };
+
+    struct image_pixels_tag {};
+
+    template<>
+    struct serializer<std::vector<std::byte>, image_pixels_tag> {
+        serializer(image_pixels_tag) {}
+
+        json operator()(const std::vector<std::byte> &value) const {
+            return base64::base64_encode(value);
         }
     };
 
     template<typename Context>
-    struct deserializer<utils::image_pixels, Context> {
-        std::vector<std::byte> deserialize_base64_string(const json &value) const {
+    struct serializer<utils::image_pixels, Context>
+        : aggregate_serializer_unchecked<utils::image_pixels, image_pixels_tag>
+    {
+        serializer() : aggregate_serializer_unchecked(image_pixels_tag{}) {}
+    };
+
+    template<>
+    struct deserializer<std::vector<std::byte>, image_pixels_tag> {
+        deserializer(image_pixels_tag) {}
+        
+        std::vector<std::byte> operator()(const json &value) const {
             if (!value.is_string()) {
                 throw std::runtime_error("Cannot deserialize base64 encoded string");
             }
             return base64::base64_decode(value.get<std::string>());
         }
+    };
 
+    template<typename Context>
+    struct deserializer<utils::image_pixels, Context> {
         utils::image_pixels operator()(const json &value) const {
-            if (!value.is_object()) {
-                throw std::runtime_error("Cannot deserialize image_pixels: value is not object");
-            }
-            utils::image_pixels result {
-                value.contains("width") ? deserializer<int>{}(value["width"]) : 0,
-                value.contains("height") ? deserializer<int>{}(value["height"]) : 0,
-                value.contains("pixels") ? deserialize_base64_string(value["pixels"]) : std::vector<std::byte>{}
-            };
+            aggregate_deserializer_unchecked<utils::image_pixels, image_pixels_tag> base{image_pixels_tag{}};
+            utils::image_pixels result = base(value);
             if (result.pixels.size() != result.width * result.height * 4) {
                 throw std::runtime_error("Invalid image");
             }
