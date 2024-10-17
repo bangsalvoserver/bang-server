05e727d7ad9c64e06ef5de423001db3281b8c617
Salvo Miosi <salvo.ilmiosi@gmail.com>
2024-10-17 22:46:43 +0200

    add bot pick_prompt for thedaltons

--- src/effects/highnoon/thedaltons.cpp
+++ src/effects/highnoon/thedaltons.cpp
@@ -4,6 +4,8 @@
 
 #include "game/game.h"
 
+#include "cards/filter_enums.h"
+
 namespace banggame {
 
     struct request_thedaltons : request_picking {
@@ -24,6 +26,15 @@ namespace banggame {
             target->m_game->pop_request();
             target->m_game->add_log("LOG_DISCARDED_CARD_FOR", origin_card, target, target_card);
             target->discard_card(target_card);
+        }
+
+        prompt_string pick_prompt(card_ptr target_card) const override {
+            if (target->is_bot()) {
+                if (target_card->has_tag(tag_type::ghost_card) || target_card->self_equippable()) {
+                    return "BOT_BAD_PLAY";
+                }
+            }
+            return {};
         }
 
         game_string status_text(player_ptr owner) const override {
