18d39b5340fc4ae67b5ac83181e2efac0ee8c472
Salvo Miosi <salvo.ilmiosi@gmail.com>
2024-11-15 10:13:29 +0100

    use event_type::check_play_card for reverend and big spencer

--- src/effects/highnoon/reverend.cpp
+++ src/effects/highnoon/reverend.cpp
@@ -6,13 +6,12 @@
 namespace banggame {
 
     void equip_reverend::on_enable(card_ptr target_card, player_ptr target) {
-        target->m_game->add_disabler(target_card, [](const_card_ptr c) {
-            return c->pocket == pocket_type::player_hand
-                && c->has_tag(tag_type::beer);
+        target->m_game->add_listener<event_type::check_play_card>(target_card, [=](player_ptr origin, card_ptr origin_card, const effect_context &ctx, game_string &out_error) {
+            if (origin_card->pocket == pocket_type::player_hand
+                && origin_card->has_tag(tag_type::beer)
+            ) {
+                out_error = {"ERROR_CARD_DISABLED_BY", origin_card, target_card};
+            }
         });
-    }
-
-    void equip_reverend::on_disable(card_ptr target_card, player_ptr target) {
-        target->m_game->remove_disablers(target_card);
     }
 }
