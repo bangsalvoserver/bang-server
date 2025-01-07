5d06defae5fad71951aae41589a5e4e8d5c0b2dc
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-01-07 11:05:41 +0100

    fix wilhelm scream

--- src/effects/legends/wilhelm_scream.cpp
+++ src/effects/legends/wilhelm_scream.cpp
@@ -10,7 +10,7 @@ namespace banggame {
 
     void equip_wilhelm_scream::on_enable(card_ptr origin_card, player_ptr target) {
         target->m_game->add_listener<event_type::apply_bang_modifier>(origin_card, [=](player_ptr origin, shared_request_bang req) {
-            if (origin == target->m_game->m_playing && origin->m_game->calc_distance(origin, req->target) >= 2) {
+            if (origin == target->m_game->m_playing && origin->m_game->calc_distance(origin, req->target) + req->target->get_distance_mod() >= 2) {
                 queue_request_perform_feat(origin_card, origin);
             }
         });
