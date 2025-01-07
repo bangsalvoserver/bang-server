4146559b3b3e3c8fbc7c81e62cdf1b74623346a2
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-01-07 13:04:34 +0100

    fix train_to_yooma priority

--- src/effects/legends/perform_feat.cpp
+++ src/effects/legends/perform_feat.cpp
@@ -78,8 +78,8 @@ namespace banggame {
     }
 
     struct request_perform_feat : request_resolvable, interface_picking {
-        request_perform_feat(card_ptr origin_card, player_ptr target)
-            : request_resolvable{origin_card, nullptr, target, {}, 30} {}
+        request_perform_feat(card_ptr origin_card, player_ptr target, int priority)
+            : request_resolvable{origin_card, nullptr, target, {}, priority} {}
         
         void on_update() override {
             if (!target->alive() || !effect_perform_feat{}.can_play(origin_card, target)) {
@@ -128,8 +128,8 @@ namespace banggame {
         }
     };
 
-    void queue_request_perform_feat(card_ptr origin_card, player_ptr target) {
-        target->m_game->queue_request<request_perform_feat>(origin_card, target);
+    void queue_request_perform_feat(card_ptr origin_card, player_ptr target, int priority) {
+        target->m_game->queue_request<request_perform_feat>(origin_card, target, priority);
     }
 
     void effect_perform_feat::on_play(card_ptr origin_card, player_ptr origin) {
