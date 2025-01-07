217b74e9efe253f2153d5cff3c71ed024fedecfa
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-01-07 16:08:55 +0100

    fix last_hero

--- src/effects/legends/last_hero.cpp
+++ src/effects/legends/last_hero.cpp
@@ -8,7 +8,7 @@ namespace banggame {
 
     void equip_last_hero::on_enable(card_ptr origin_card, player_ptr target) {
         target->m_game->add_listener<event_type::on_destroy_card>(origin_card, [=](player_ptr origin, card_ptr target_card, bool is_destroyed, bool &handled) {
-            if (origin == target->m_game->m_playing && is_destroyed && target_card->is_blue()) {
+            if (origin == target->m_game->m_playing && is_destroyed && target_card->pocket == pocket_type::player_table && target_card->is_blue()) {
                 queue_request_perform_feat(origin_card, origin);
             }
         });
