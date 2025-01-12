ec40c4a035042471e59d43b8ad0ed45c8e1dc1ec
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-01-12 15:04:18 +0100

    fix fistfulofcards + barrel + john_pain

--- src/effects/fistfulofcards/fistfulofcards.cpp
+++ src/effects/fistfulofcards/fistfulofcards.cpp
@@ -8,12 +8,16 @@
 
 namespace banggame {
     
-    void equip_fistfulofcards::on_enable(card_ptr target_card, player_ptr target) {
-        target->m_game->add_listener<event_type::pre_turn_start>(target_card, [=](player_ptr p) {
-            if (!p->empty_hand()) {
-                p->m_game->add_log("LOG_RECEIVED_N_BANGS_FOR", p, target_card, int(p->m_hand.size()));
-                for (int i=0; i<p->m_hand.size(); ++i) {
-                    p->m_game->queue_request<request_bang>(target_card, nullptr, p, effect_flags{}, -1);
+    void equip_fistfulofcards::on_enable(card_ptr origin_card, player_ptr origin) {
+        origin->m_game->add_listener<event_type::pre_turn_start>(origin_card, [=](player_ptr target) {
+            if (!target->empty_hand()) {
+                target->m_game->add_log("LOG_RECEIVED_N_BANGS_FOR", target, origin_card, int(target->m_hand.size()));
+                for (int i = 0; i < target->m_hand.size(); ++i) {
+                    target->m_game->queue_action([=]{
+                        if (target->alive()) {
+                            target->m_game->queue_request<request_bang>(origin_card, nullptr, target);
+                        }
+                    }, -1);
                 }
             }
         });
