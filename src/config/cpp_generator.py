10c0106754b692cb4bc391f2dc2e1a1c9416961b
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-01-26 23:09:16 +0100

    use match/case in cpp_generator

--- src/config/cpp_generator.py
+++ src/config/cpp_generator.py
@@ -87,68 +87,72 @@ def convert_declaration(declaration: CppDeclaration, indent = 0):
         return span_name
 
     def traverse_declaration(object_value):
-        if isinstance(object_value, CppObject):
-            return CppObject(**{
-                key: traverse_declaration(value) for key, value in object_value.value.items()
-            })
-        elif isinstance(object_value, CppSpan):
-            if not object_value.value: return []
-            return CppLiteral(get_extra_declaration_name(traverse_declaration(object_value.value), object_value.type_name))
-        elif isinstance(object_value, CppStaticMap):
-            return CppLiteral(get_extra_declaration_name(CppStaticMap(
-                key_type=object_value.key_type,
-                value_type=object_value.value_type,
-                value=traverse_declaration(object_value.value)
-            ), object_type=(object_value.key_type, object_value.value_type)))
-        elif isinstance(object_value, list):
-            return [traverse_declaration(value) for value in object_value]
-        elif isinstance(object_value, tuple):
-            return tuple(traverse_declaration(value) for value in object_value)
-        elif isinstance(object_value, bytes):
-            return CppLiteral(get_extra_declaration_name(object_value, 'uint8_t'))
-        else:
-            return object_value
+        match object_value:
+            case CppObject():
+                return CppObject(**{
+                    key: traverse_declaration(value) for key, value in object_value.value.items()
+                })
+            case CppSpan():
+                if not object_value.value: return []
+                return CppLiteral(get_extra_declaration_name(traverse_declaration(object_value.value), object_value.type_name))
+            case CppStaticMap():
+                return CppLiteral(get_extra_declaration_name(CppStaticMap(
+                    key_type=object_value.key_type,
+                    value_type=object_value.value_type,
+                    value=traverse_declaration(object_value.value)
+                ), object_type=(object_value.key_type, object_value.value_type)))
+            case list():
+                return [traverse_declaration(value) for value in object_value]
+            case tuple():
+                return tuple(traverse_declaration(value) for value in object_value)
+            case bytes():
+                return CppLiteral(get_extra_declaration_name(object_value, 'uint8_t'))
+            case _:
+                return object_value
     
     def gen_extra_declaration(name, type_name, value):
-        if isinstance(value, CppStaticMap):
-            return CppDeclaration(
-                object_name=f"static const auto {name}",
-                object_value=value
-            )
-        return CppDeclaration(
-            object_name=f"static const {type_name} {name}[]",
-            object_value=value
-        )
+        match value:
+            case CppStaticMap():
+                return CppDeclaration(
+                    object_name=f"static const auto {name}",
+                    object_value=value
+                )
+            case _:
+                return CppDeclaration(
+                    object_name=f"static const {type_name} {name}[]",
+                    object_value=value
+                )
 
     def object_to_string(object_value, indent = 0):
-        if isinstance(object_value, CppDeclaration):
-            return f"{SPACE * indent}{object_value.object_name} {object_to_string(object_value.object_value, indent)};\n"
-        elif isinstance(object_value, CppObject):
-            if not object_value.value: return '{}'
-            return '{\n' + ',\n'.join(f"{SPACE * (indent + 1)}.{key} {object_to_string(value, indent + 1)}" for key, value in object_value.value.items() if value is not None) + '\n' + (SPACE * indent) + '}'
-        elif isinstance(object_value, dict):
-            if not object_value: return '{}'
-            return '{\n' + ',\n'.join(f"{SPACE * (indent + 1)}{{{object_to_string(key)}, {object_to_string(value)}}}" for key, value in object_value.items()) + '\n' + (SPACE * indent) + '}'
-        elif isinstance(object_value, list):
-            if not object_value: return '{}'
-            return '{\n' + ',\n'.join(f"{SPACE * (indent + 1)}{object_to_string(value, indent + 1)}" for value in object_value if value is not None) + '\n' + (SPACE * indent) + '}'
-        elif isinstance(object_value, tuple):
-            if not object_value: return '{}'
-            return '{ ' + ', '.join(f"{object_to_string(value, indent)}" for value in object_value) + ' }'
-        elif isinstance(object_value, CppStaticMap):
-            if not object_value: return '{}'
-            return f'{{utils::make_static_map<{object_value.key_type}, {object_value.value_type}>({{\n' + \
-                ',\n'.join(f"{SPACE * (indent + 1)}{object_to_string(pair, indent + 1)}" for pair in object_value.value.items()) + \
-                '\n' + (SPACE * indent) + '})}'
-        elif isinstance(object_value, str):
-            return f'{{\"{object_value}\"sv}}'
-        elif isinstance(object_value, bool):
-            return f"{{{'true' if object_value else 'false'}}}"
-        elif isinstance(object_value, bytes):
-            if not object_value: return '{}'
-            return '{\n' + ',\n'.join(SPACE * (indent + 1) + ','.join(f'0x{byte:02x}' for byte in line) for line in chunks(object_value, 32)) + '\n' + (SPACE * indent) + '}'
-        else:
-            return f'{{{object_value}}}'
+        match object_value:
+            case CppDeclaration():
+                return f"{SPACE * indent}{object_value.object_name} {object_to_string(object_value.object_value, indent)};\n"
+            case CppObject():
+                if not object_value.value: return '{}'
+                return '{\n' + ',\n'.join(f"{SPACE * (indent + 1)}.{key} {object_to_string(value, indent + 1)}" for key, value in object_value.value.items() if value is not None) + '\n' + (SPACE * indent) + '}'
+            case dict():
+                if not object_value: return '{}'
+                return '{\n' + ',\n'.join(f"{SPACE * (indent + 1)}{{{object_to_string(key)}, {object_to_string(value)}}}" for key, value in object_value.items()) + '\n' + (SPACE * indent) + '}'
+            case list():
+                if not object_value: return '{}'
+                return '{\n' + ',\n'.join(f"{SPACE * (indent + 1)}{object_to_string(value, indent + 1)}" for value in object_value if value is not None) + '\n' + (SPACE * indent) + '}'
+            case tuple():
+                if not object_value: return '{}'
+                return '{ ' + ', '.join(f"{object_to_string(value, indent)}" for value in object_value) + ' }'
+            case CppStaticMap():
+                if not object_value: return '{}'
+                return f'{{utils::make_static_map<{object_value.key_type}, {object_value.value_type}>({{\n' + \
+                    ',\n'.join(f"{SPACE * (indent + 1)}{object_to_string(pair, indent + 1)}" for pair in object_value.value.items()) + \
+                    '\n' + (SPACE * indent) + '})}'
+            case str():
+                return f'{{\"{object_value}\"sv}}'
+            case bool():
+                return f"{{{'true' if object_value else 'false'}}}"
+            case bytes():
+                if not object_value: return '{}'
+                return '{\n' + ',\n'.join(SPACE * (indent + 1) + ','.join(f'0x{byte:02x}' for byte in line) for line in chunks(object_value, 32)) + '\n' + (SPACE * indent) + '}'
+            case _:
+                return f'{{{object_value}}}'
     
     transformed = CppDeclaration(
         object_name=declaration.object_name,
@@ -160,17 +164,10 @@ def convert_declaration(declaration: CppDeclaration, indent = 0):
         object_to_string(decl, indent)
         for decl in [*(gen_extra_declaration(*tup) for tup in extra_declarations), transformed]
     )
-
-def as_list(value):
-    if isinstance(value, list):
-        for elem in value:
-            yield elem
-    else:
-        yield value
         
 def print_cpp_file(declaration: CppDeclaration, include_filenames = None, file=sys.stdout):
     print("// AUTO GENERATED FILE\n", file=file)
-    for filename in as_list(include_filenames):
+    for filename in (include_filenames if isinstance(include_filenames, list) else [include_filenames]):
         if filename.startswith('<'):
             print(f"#include {filename}\n", file=file)
         else:
