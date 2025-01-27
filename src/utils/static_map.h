b65143c8fb23e39bb82656ae5707e2523940c5e9
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-01-27 12:03:12 +0100

    rework static_map

--- src/utils/static_map.h
+++ src/utils/static_map.h
@@ -10,8 +10,30 @@ namespace utils {
     template<typename Key, typename Value, typename Comp = std::less<>>
     class static_map_view;
 
+    template<typename Comp>
+    class static_map_base : private Comp {
+    public:
+        constexpr static_map_base(Comp comp = {}) : Comp(std::move(comp)) {}
+
+        constexpr const Comp &comparator() const {
+            return static_cast<const Comp &>(*this);
+        }
+
+        constexpr auto find(this const auto &self, const auto &key) {
+            auto begin = self.begin();
+            auto end = self.end();
+            auto it = std::ranges::lower_bound(begin, end, key, self.comparator(), [](const auto &pair) {
+                return pair.first;
+            });
+            if (it != end && it->first == key) {
+                return it;
+            }
+            return end;
+        }
+    };
+
     template<typename Key, typename Value, size_t Size, typename Comp = std::less<>>
-    class static_map {
+    class static_map : public static_map_base<Comp> {
     public:
         using value_type = std::pair<Key, Value>;
 
@@ -19,28 +41,22 @@ namespace utils {
         std::array<value_type, Size> m_data;
 
     public:
-        constexpr static_map(value_type (&&data)[Size]) {
+        constexpr static_map(value_type (&&data)[Size], Comp &&comp)
+            : static_map_base<Comp>{std::move(comp)}
+        {
             std::ranges::move(data, m_data.begin());
-            std::ranges::sort(m_data, Comp{}, &value_type::first);
+            std::ranges::sort(m_data, this->comparator(), &value_type::first);
             assert(std::ranges::empty(std::ranges::unique(m_data, {}, &value_type::first)) && "Keys must be unique");
         }
 
         constexpr auto begin() const { return m_data.begin(); }
         constexpr auto end() const { return m_data.end(); }
 
-        constexpr auto find(const auto &key) const {
-            if (auto it = std::ranges::lower_bound(m_data, key, Comp{}, &value_type::first); it != end() && it->first == key) {
-                return it;
-            } else {
-                return end();
-            }
-        }
-
         friend class static_map_view<Key, Value, Comp>;
     };
 
     template<typename Key, typename Value, typename Comp>
-    class static_map_view {
+    class static_map_view : public static_map_base<Comp> {
     public:
         using value_type = const std::pair<Key, Value>;
     
@@ -48,27 +64,21 @@ namespace utils {
         std::span<value_type> m_span;
     
     public:
-        constexpr static_map_view() = default;
+        constexpr static_map_view(Comp comp = {})
+            : static_map_base<Comp>(std::move(comp)) {}
         
         template<size_t Size>
         constexpr static_map_view(const static_map<Key, Value, Size, Comp> &map)
-            : m_span{map.m_data} {}
+            : static_map_base<Comp>{map.comparator()}
+            , m_span{map.m_data} {}
 
         constexpr auto begin() const { return m_span.begin(); }
         constexpr auto end() const { return m_span.end(); }
-
-        constexpr auto find(const auto &key) const {
-            if (auto it = std::ranges::lower_bound(m_span, key, Comp{}, &value_type::first); it != end() && it->first == key) {
-                return it;
-            } else {
-                return end();
-            }
-        }
     };
 
     template<typename Key, typename Value, size_t Size, typename Comp = std::less<>>
     constexpr auto make_static_map(std::pair<Key, Value> (&&data)[Size], Comp comp = {}) {
-        return static_map<Key, Value, Size, Comp>(std::move(data));
+        return static_map<Key, Value, Size, Comp>(std::move(data), std::move(comp));
     }
 
 }
