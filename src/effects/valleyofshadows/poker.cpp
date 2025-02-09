80c74d705355b7cbb33731eb48c67dbda06ee3b0
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-02-09 13:56:44 +0100

    rework poker

--- src/effects/valleyofshadows/poker.cpp
+++ src/effects/valleyofshadows/poker.cpp
@@ -11,10 +11,14 @@ namespace banggame {
 
     struct request_poker : request_picking, escapable_request {
         request_poker(card_ptr origin_card, player_ptr origin, player_ptr target, effect_flags flags = {})
-            : request_picking(origin_card, origin, target, flags) {}
+            : request_picking(origin_card, origin, target, flags, 100) {}
 
         void on_update() override {
-            auto_pick();
+            if (target->immune_to(origin_card, origin, flags)) {
+                target->m_game->pop_request();
+            } else {
+                auto_pick();
+            }
         }
 
         bool can_pick(const_card_ptr target_card) const override {
@@ -23,12 +27,12 @@ namespace banggame {
 
         void on_pick(card_ptr target_card) override {
             target->m_game->pop_request();
-            if (!target->immune_to(origin_card, origin, flags)) {
-                target->m_game->add_log(update_target::includes(origin, target), "LOG_DISCARDED_CARD_FOR", origin_card, target, target_card);
-                target->m_game->add_log(update_target::excludes(origin, target), "LOG_DISCARDED_A_CARD_FOR", origin_card, target);
-                target_card->move_to(pocket_type::selection, origin);
+
+            target->m_game->queue_action([=, origin_card=origin_card, target=target]{
+                target->m_game->add_log("LOG_DISCARDED_CARD_FOR", origin_card, target, target_card);
+                target_card->move_to(pocket_type::selection);
                 target->m_game->call_event(event_type::on_discard_hand_card{ target, target_card, true });
-            }
+            }, 95);
         }
 
         game_string status_text(player_ptr owner) const override {
@@ -42,17 +46,12 @@ namespace banggame {
 
     struct request_poker_draw : selection_picker {
         request_poker_draw(card_ptr origin_card, player_ptr target)
-            : selection_picker(origin_card, nullptr, target) {}
+            : selection_picker(origin_card, nullptr, target, {}, 90) {}
 
         int num_cards = 2;
 
         void on_update() override {
             if (live) return;
-            
-            for (card_ptr target_card : target->m_game->m_selection) {
-                target->m_game->add_log("LOG_POKER_REVEAL", origin_card, target_card);
-                target_card->set_visibility(card_visibility::shown);
-            }
             
             if (auto aces = rv::filter(target->m_game->m_selection, [this](card_ptr c) {
                 return c->get_modified_sign().rank == card_rank::rank_A;
