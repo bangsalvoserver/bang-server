ff57c948cc1a271f71947c8eeed2a59ec8d77e6e
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-02-19 16:20:53 +0100

    fix greygory deck

--- src/effects/wildwestshow/greygory_deck.cpp
+++ src/effects/wildwestshow/greygory_deck.cpp
@@ -22,8 +22,10 @@ namespace banggame {
     void effect_greygory_deck::on_play(card_ptr origin_card, player_ptr origin) {
         origin->remove_cards({origin->m_characters.begin() + 1, origin->m_characters.end()});
         card_list characters = origin->m_game->m_characters
-            | rv::filter([](card_ptr c) {
-                return c->pocket == pocket_type::none && c->owner == nullptr && c->expansion.empty();
+            | rv::filter([=](card_ptr c) {
+                return c->pocket == pocket_type::none &&
+                    (c->owner == nullptr || c->owner == origin)
+                    && c->expansion.empty();
             })
             | rv::sample(2, origin->m_game->rng)
             | rn::to_vector;
