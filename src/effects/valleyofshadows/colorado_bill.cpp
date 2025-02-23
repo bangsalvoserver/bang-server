568b05c07af94aa1320615bcdb2402eecae339b5
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-02-23 13:59:24 +0100

    add colorado_bill2

--- src/effects/valleyofshadows/colorado_bill.cpp
+++ src/effects/valleyofshadows/colorado_bill.cpp
@@ -1,8 +1,11 @@
 #include "colorado_bill.h"
 
-#include "game/game_table.h"
+#include "cards/game_enums.h"
+
 #include "effects/base/bang.h"
 #include "effects/base/draw_check.h"
+
+#include "game/game_table.h"
 
 namespace banggame {
     
@@ -13,6 +16,21 @@ namespace banggame {
                     if (result) {
                         p->m_game->add_log("LOG_CARD_HAS_EFFECT", target_card);
                         req->unavoidable = true;
+                    }
+                });
+            }
+        });
+    }
+    
+    void equip_colorado_bill2::on_enable(card_ptr target_card, player_ptr p) {
+        p->m_game->add_listener<event_type::on_missed>(target_card, [=](card_ptr origin_card, player_ptr origin, player_ptr target, card_ptr missed_card, effect_flags flags) {
+            if (p == origin && flags.check(effect_flag::is_bang) && flags.check(effect_flag::is_missed)) {
+                origin->m_game->queue_request<request_check>(origin, target_card, &card_sign::is_spades, [=](bool result) {
+                    if (result) {
+                        if (auto req = p->m_game->top_request<request_bang>()) {
+                            p->m_game->add_log("LOG_CARD_HAS_EFFECT", target_card);
+                            req->unavoidable = true;
+                        }
                     }
                 });
             }
--- src/effects/valleyofshadows/colorado_bill.h
+++ src/effects/valleyofshadows/colorado_bill.h
@@ -10,6 +10,12 @@ namespace banggame {
     };
 
     DEFINE_EQUIP(colorado_bill, equip_colorado_bill)
+
+    struct equip_colorado_bill2 : event_equip {
+        void on_enable(card_ptr target_card, player_ptr target);
+    };
+
+    DEFINE_EQUIP(colorado_bill2, equip_colorado_bill2)
 }
 
 #endif
