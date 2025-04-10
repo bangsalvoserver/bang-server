89c33c3edf1e19eed37a3dc5ac4eca728addc0f9
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-04-10 09:37:20 +0200

    add error handling in effect_barrel::on_play

--- src/effects/base/barrel.cpp
+++ src/effects/base/barrel.cpp
@@ -11,13 +11,16 @@ namespace banggame {
     }
     
     void effect_barrel::on_play(card_ptr origin_card, player_ptr target) {
-        auto req = target->m_game->top_request<missable_request>();
-        req->add_card(origin_card);
-        target->m_game->queue_request<request_check>(target, origin_card, &card_sign::is_hearts, [=](bool result) {
-            if (result) {
-                target->m_game->add_log("LOG_CARD_HAS_EFFECT", origin_card);
-                effect_missed{}.on_play(origin_card, target);
-            }
-        });
+        if (auto req = target->m_game->top_request<missable_request>()) {
+            req->add_card(origin_card);
+            target->m_game->queue_request<request_check>(target, origin_card, &card_sign::is_hearts, [=](bool result) {
+                if (result) {
+                    target->m_game->add_log("LOG_CARD_HAS_EFFECT", origin_card);
+                    effect_missed{}.on_play(origin_card, target);
+                }
+            });
+        } else {
+            throw game_error("invalid request access in effect_barrel: top_request is not missable_request");
+        }
     }
 }
