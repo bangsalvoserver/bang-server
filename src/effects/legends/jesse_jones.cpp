5734a46859fa1c37cf59ac2a62d483f320597dcb
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-04-14 12:48:07 +0200

    bugfix jesse jones legend + sacagaway

--- src/effects/legends/jesse_jones.cpp
+++ src/effects/legends/jesse_jones.cpp
@@ -12,19 +12,26 @@
 namespace banggame {
 
     struct request_jesse_jones_legend : request_picking {
-        using request_picking::request_picking;
+        request_jesse_jones_legend(card_ptr origin_card, player_ptr origin, player_ptr target)
+            : request_picking(origin_card, origin, target, {}, 39) {}
 
         void on_update() override {
-            if (update_count == 0) {
-                if (origin->m_hand.size() == 1) {
-                    on_pick(origin->m_hand.front());
-                } else {
+            if (!target->m_game->check_flags(game_flag::hands_shown)) {
+                if (update_count == 0) {
                     origin->add_player_flags(player_flag::show_hand_playing);
 
                     for (card_ptr target_card : origin->m_hand) {
                         target->m_game->add_log(update_target::includes(origin, target), "LOG_REVEALED_CARD", origin, target_card);
                         target_card->set_visibility(update_target::includes(origin, target));
                     }
+                } else {
+                    target->m_game->pop_request();
+        
+                    for (card_ptr target_card : origin->m_hand) {
+                        target_card->set_visibility(card_visibility::show_owner, origin);
+                    }
+                    
+                    origin->remove_player_flags(player_flag::show_hand_playing);
                 }
             }
         }
@@ -34,17 +41,7 @@ namespace banggame {
         }
         
         void on_pick(card_ptr target_card) override {
-            target->m_game->pop_request();
-
             effect_steal{}.on_play(origin_card, target, target_card);
-
-            target->m_game->queue_action([origin=origin]{
-                for (card_ptr target_card : origin->m_hand) {
-                    target_card->set_visibility(card_visibility::show_owner, origin);
-                }
-                
-                origin->remove_player_flags(player_flag::show_hand_playing);
-            }, 39);
         }
 
         game_string status_text(player_ptr owner) const override {
@@ -62,6 +59,10 @@ namespace banggame {
     }
 
     void effect_jesse_jones_legend::on_play(card_ptr origin_card, player_ptr origin, card_ptr target) {
-        origin->m_game->queue_request<request_jesse_jones_legend>(origin_card, target->owner, origin);
+        if (target->owner->m_hand.size() == 1) {
+            effect_steal{}.on_play(origin_card, origin, target);
+        } else {
+            origin->m_game->queue_request<request_jesse_jones_legend>(origin_card, target->owner, origin);
+        }
     }
 }
