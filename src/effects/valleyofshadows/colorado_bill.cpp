a5b7c157c51d443743b67c68b7aafe7721bb8bd5
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-07-01 11:51:05 +0200

    colorado bill : indifferent if already unavoidable

--- src/effects/valleyofshadows/colorado_bill.cpp
+++ src/effects/valleyofshadows/colorado_bill.cpp
@@ -13,7 +13,7 @@ namespace banggame {
     void equip_colorado_bill::on_enable(card_ptr origin_card, player_ptr origin) {
         origin->m_game->add_listener<event_type::apply_bang_modifier>(origin_card, [=](player_ptr e_origin, shared_request_bang req) {
             if (e_origin == origin) {
-                bool indifferent = req->target->empty_hand() && count_missed_cards(req->target) == 0;
+                bool indifferent = req->unavoidable || (req->target->empty_hand() && count_missed_cards(req->target) == 0);
                 origin->m_game->queue_request<request_check>(origin, origin_card, [=](card_ptr target_card) {
                     return draw_check_result {
                         .lucky = get_modified_sign(target_card).is_spades(),
