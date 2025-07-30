8055e136a17e1bd517e4af5a3653a4be6c2773d5
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-07-30 08:31:22 +0200

    simplify select_cubes_optional

--- src/target_types/select_cubes_optional.cpp
+++ src/target_types/select_cubes_optional.cpp
@@ -6,19 +6,14 @@ namespace banggame {
 
     card_list targeting_select_cubes_optional::random_target(const effect_context &ctx) {
         if (count_cubes(origin) >= effect.target_value) {
-            return targeting_select_cubes{*this}.random_target(ctx);
+            return targeting_select_cubes::random_target(ctx);
         }
         return {};
     }
 
     game_string targeting_select_cubes_optional::get_error(const effect_context &ctx, const card_list &target_cards) {
-        if (!target_cards.empty() && target_cards.size() != effect.target_value) {
-            return "ERROR_INVALID_TARGETS";
-        }
-        for (card_ptr c : target_cards) {
-            if (c->owner != origin) {
-                return {"ERROR_TARGET_NOT_SELF", origin_card};
-            }
+        if (!target_cards.empty()) {
+            return targeting_select_cubes::get_error(ctx, target_cards);
         }
         return {};
     }
