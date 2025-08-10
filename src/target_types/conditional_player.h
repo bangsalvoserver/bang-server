02a2983f73976510b33b04d54abc0dd7c06495a7
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-08-10 10:33:17 +0200

    move extra_card and conditional_player impl in header

--- src/target_types/conditional_player.h
+++ src/target_types/conditional_player.h
@@ -10,12 +10,53 @@ namespace banggame {
 
         using value_type = nullable_player;
         
-        std::generator<player_ptr> possible_targets(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx);
-        player_ptr random_target(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx);
-        game_string get_error(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, player_ptr target);
-        prompt_string on_prompt(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, player_ptr target);
-        void add_context(card_ptr origin_card, player_ptr origin, const effect_holder &effect, effect_context &ctx, player_ptr target);
-        void on_play(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, player_ptr target);
+        std::generator<player_ptr> possible_targets(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx) {
+            if (auto targets = targeting_player::possible_targets(origin_card, origin, effect, ctx)) {
+                for (player_ptr target : targets) {
+                    co_yield target;
+                }
+            } else {
+                co_yield nullptr;
+            }
+        }
+
+        player_ptr random_target(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx) {
+            if (auto targets = targeting_player::possible_targets(origin_card, origin, effect, ctx)) {
+                return random_element(targets, origin->m_game->bot_rng);
+            } else {
+                return nullptr;
+            }
+        }
+
+        game_string get_error(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, player_ptr target) {
+            if (target) {
+                return targeting_player::get_error(origin_card, origin, effect, ctx, target);
+            } else if (bool(targeting_player::possible_targets(origin_card, origin, effect, ctx))) {
+                return "ERROR_TARGET_SET_NOT_EMPTY";
+            } else {
+                return {};
+            }
+        }
+
+        prompt_string on_prompt(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, player_ptr target) {
+            if (target) {
+                return targeting_player::on_prompt(origin_card, origin, effect, ctx, target);
+            } else {
+                return {};
+            }
+        }
+
+        void add_context(card_ptr origin_card, player_ptr origin, const effect_holder &effect, effect_context &ctx, player_ptr target) {
+            if (target) {
+                targeting_player::add_context(origin_card, origin, effect, ctx, target);
+            }
+        }
+
+        void on_play(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, player_ptr target) {
+            if (target) {
+                targeting_player::on_play(origin_card, origin, effect, ctx, target);
+            }
+        }
     };
 
     DEFINE_TARGETING(conditional_player, targeting_conditional_player)
--- src/target_types/extra_card.h
+++ src/target_types/extra_card.h
@@ -9,13 +9,60 @@ namespace banggame {
         using targeting_card::targeting_card;
 
         using value_type = nullable_card;
-        
-        std::generator<card_ptr> possible_targets(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx);
-        card_ptr random_target(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx);
-        game_string get_error(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, card_ptr target);
-        prompt_string on_prompt(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, card_ptr target);
-        void add_context(card_ptr origin_card, player_ptr origin, const effect_holder &effect, effect_context &ctx, card_ptr target);
-        void on_play(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, card_ptr target);
+
+        std::generator<card_ptr> possible_targets(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx) {
+            if (ctx.repeat_card) {
+                co_yield nullptr;
+            } else {
+                for (card_ptr target : targeting_card::possible_targets(origin_card, origin, effect, ctx)) {
+                    co_yield target;
+                }
+            }
+        }
+
+        card_ptr random_target(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx) {
+            if (ctx.repeat_card) {
+                return nullptr;
+            } else {
+                return targeting_card::random_target(origin_card, origin, effect, ctx);
+            }
+        }
+
+        game_string get_error(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, card_ptr target) {
+            if (!target) {
+                if (ctx.repeat_card) {
+                    return {};
+                } else {
+                    return "ERROR_TARGET_SET_NOT_EMPTY";
+                }
+            } else {
+                if (ctx.repeat_card) {
+                    return "ERROR_TARGET_SET_EMPTY";
+                } else {
+                    return targeting_card::get_error(origin_card, origin, effect, ctx, target);
+                }
+            }
+        }
+
+        prompt_string on_prompt(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, card_ptr target) {
+            if (target) {
+                return targeting_card::on_prompt(origin_card, origin, effect, ctx, target);
+            } else {
+                return {};
+            }
+        }
+
+        void add_context(card_ptr origin_card, player_ptr origin, const effect_holder &effect, effect_context &ctx, card_ptr target) {
+            if (target) {
+                targeting_card::add_context(origin_card, origin, effect, ctx, target);
+            }
+        }
+
+        void on_play(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, card_ptr target) {
+            if (target) {
+                return targeting_card::on_play(origin_card, origin, effect, ctx, target);
+            }
+        }
     };
 
     DEFINE_TARGETING(extra_card, targeting_extra_card)
