2915fccd12689295fa7087eb54662eb77b3a6013
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-10-31 18:02:47 +0100

    use weapon tag for bot prompts

--- src/effects/base/weapon.cpp
+++ src/effects/base/weapon.cpp
@@ -8,33 +8,37 @@
 
 namespace banggame {
 
-    struct is_weapon {
-        const_card_ptr target_card;
-        bool operator ()(const_card_ptr c) const {
-            return c != target_card && c->has_tag(tag_type::weapon);
+    static card_ptr find_weapon_card(card_ptr origin_card, player_ptr target) {
+        for (card_ptr target_card : target->m_table) {
+            if (origin_card != target_card && target_card->has_tag(tag_type::weapon)) {
+                return target_card;
+            }
         }
+        return nullptr;
     };
 
     game_string equip_weapon::on_prompt(card_ptr origin_card, player_ptr origin, player_ptr target) {
         if (range == 0) {
             MAYBE_RETURN(prompts::bot_check_target_enemy(origin, target));
             return prompts::prompt_target_self(origin_card, origin, target);
-        } else if (!origin->is_bot() && target == origin && origin->get_weapon_range() != 0) {
-            if (auto it = rn::find_if(target->m_table, is_weapon{origin_card}); it != target->m_table.end()) {
-                return {"PROMPT_REPLACE", origin_card, *it};
+        } else if (target == origin && origin->get_weapon_range() != 0) {
+            if (card_ptr target_card = find_weapon_card(origin_card, target)) {
+                if (!target->is_bot() || origin_card->get_tag_value(tag_type::weapon) >= target_card->get_tag_value(tag_type::weapon)) {
+                    return {"PROMPT_REPLACE", origin_card, target_card};
+                }
             }
         }
         return {};
     }
 
-    void equip_weapon::on_enable(card_ptr target_card, player_ptr target) {
-        if (auto it = rn::find_if(target->m_table, is_weapon{target_card}); it != target->m_table.end()) {
-            target->m_game->add_log("LOG_DISCARDED_SELF_CARD", target, *it);
-            target->discard_card(*it);
+    void equip_weapon::on_enable(card_ptr origin_card, player_ptr target) {
+        if (card_ptr target_card = find_weapon_card(origin_card, target)) {
+            target->m_game->add_log("LOG_DISCARDED_SELF_CARD", target, target_card);
+            target->discard_card(target_card);
         }
 
-        target->m_game->add_listener<event_type::count_range_mod>(target_card, [=, range=range](const_player_ptr origin, range_mod_type type, int &value) {
-            if (origin == target && type == range_mod_type::weapon_range && !origin->m_game->is_disabled(target_card)) {
+        target->m_game->add_listener<event_type::count_range_mod>(origin_card, [=, range=range](const_player_ptr origin, range_mod_type type, int &value) {
+            if (origin == target && type == range_mod_type::weapon_range && !origin->m_game->is_disabled(origin_card)) {
                 value = range;
             }
         });
