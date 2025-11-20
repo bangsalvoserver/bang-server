f9faebe9e9298275ef480b67e9c8c1adae439d5d
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-11-20 15:59:07 +0100

    fix effect_escape

--- src/effects/valleyofshadows/escape.h
+++ src/effects/valleyofshadows/escape.h
@@ -5,21 +5,21 @@
 
 namespace banggame {
 
-    struct effect_escape {
-        static bool can_escape(player_ptr origin, card_ptr origin_card, effect_flags flags);
-        
-        bool can_play(card_ptr origin_card, player_ptr origin);
+    struct effect_escape_base {
         prompt_string on_prompt(card_ptr origin_card, player_ptr origin);
         void on_play(card_ptr origin_card, player_ptr origin);
     };
 
+    struct effect_escape : effect_escape_base {
+        static bool can_escape(player_ptr origin, card_ptr origin_card, effect_flags flags);
+        bool can_play(card_ptr origin_card, player_ptr origin);
+    };
+
     DEFINE_EFFECT(escape, effect_escape)
 
-    struct effect_escape2 {
+    struct effect_escape2 : effect_escape_base {
         static bool can_escape(player_ptr origin, card_ptr origin_card, effect_flags flags);
-        
         bool can_play(card_ptr origin_card, player_ptr origin);
-        void on_play(card_ptr origin_card, player_ptr origin);
     };
 
     DEFINE_EFFECT(escape2, effect_escape2)
