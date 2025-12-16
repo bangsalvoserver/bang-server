4509999b723a0617a141ab02d38e368c2e8e7d23
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-12-16 10:28:08 +0100

    simplify ironhorse prompt

--- src/effects/greattrainrobbery/ironhorse.cpp
+++ src/effects/greattrainrobbery/ironhorse.cpp
@@ -1,7 +1,6 @@
 #include "ironhorse.h"
 
 #include "game/game_table.h"
-#include "game/prompts.h"
 
 #include "effects/base/bang.h"
 
@@ -10,18 +9,6 @@
 #include "ruleset.h"
 
 namespace banggame {
-
-    static prompt_string get_ironhorse_prompt(player_ptr origin, player_ptr target, int locomotive_count) {
-        if (origin->is_bot()) {
-            auto role = origin->get_base_role();
-            if (!(role == player_role::outlaw || role == player_role::renegade && origin->m_game->num_alive() <= 2)
-                && (target->m_hp <= locomotive_count && target->m_role == player_role::sheriff)
-            ) {
-                return {1, "BOT_DONT_KILL_SHERIFF"};
-            }
-        }
-        return {};
-    }
 
     void equip_ironhorse::on_enable(card_ptr origin_card, player_ptr origin) {
         origin->m_game->add_listener<event_type::on_locomotive_effect>(origin_card, [=](player_ptr target, shared_locomotive_context ctx) {
@@ -35,9 +22,15 @@ namespace banggame {
         });
 
         origin->m_game->add_listener<event_type::get_locomotive_prompt>(origin_card, [](player_ptr target, int locomotive_count, prompt_string &out_prompt) {
-            out_prompt = prompts::select_prompt_fallback_empty(target->m_game->range_alive_players(target) | rv::transform([&](player_ptr p){
-                return get_ironhorse_prompt(target, p, locomotive_count);
-            }));
+            if (target->is_bot()) {
+                player_ptr sheriff = target->m_game->m_first_player;
+                auto role = target->get_base_role();
+                if (!(role == player_role::outlaw || role == player_role::renegade && target->m_game->num_alive() <= 2)
+                    && (sheriff->m_hp <= locomotive_count && sheriff->m_role == player_role::sheriff)
+                ) {
+                    out_prompt = "BOT_DONT_KILL_SHERIFF";
+                }
+            }
         });
     }
 }
