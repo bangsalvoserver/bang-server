1223efa90f01d7c13412a07828a7ae3558bf010f
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-12-16 15:34:17 +0100

    throw when moving character cards

--- src/game/player.cpp
+++ src/game/player.cpp
@@ -378,7 +378,7 @@ namespace banggame {
             m_game->add_cards_to({ target_card }, pocket_type::player_character, this, card_visibility::shown);
         } else {
             if (target_card->pocket == pocket_type::player_character && target_card->owner != this) {
-                target_card->owner->disable_equip(target_card);
+                throw game_error("cannot move character card from one player to another");
             }
             target_card->move_to(pocket_type::player_character, this, card_visibility::shown);
         }
