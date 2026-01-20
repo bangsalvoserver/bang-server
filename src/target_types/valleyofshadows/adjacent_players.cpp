108c56f7f8b76b22632fe68308e5334b72bfe59d
Salvo Miosi <salvo.ilmiosi@gmail.com>
2026-01-20 10:09:41 +0100

    rework adjacent_players

--- src/target_types/valleyofshadows/adjacent_players.cpp
+++ src/target_types/valleyofshadows/adjacent_players.cpp
@@ -7,15 +7,52 @@
 
 namespace banggame {
 
-    game_string targeting_adjacent_players::get_error(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, player_pair targets) {
-        MAYBE_RETURN(check_player_filter(origin_card, origin, target_player.player_filter, targets[0], ctx));
-        if (!targets[1]->alive()) {
-            return {"ERROR_TARGET_DEAD", origin_card, targets[1]};
+    static game_string check_second_target(card_ptr origin_card, player_ptr origin, player_ptr first_target, player_ptr second_target, const effect_context &ctx, int max_distance) {
+        if (!second_target->alive()) {
+            return {"ERROR_TARGET_DEAD", origin_card, second_target};
         }
-        if (origin == targets[1]) {
+        if (origin == second_target) {
             return "ERROR_TARGET_SELF";
         }
-        if (targets[0] == targets[1] || origin->m_game->calc_distance(targets[0], targets[1]) > max_distance) {
+        if (first_target == second_target || origin->m_game->calc_distance(first_target, second_target) > max_distance) {
+            return "ERROR_TARGETS_NOT_ADJACENT";
+        }
+        return {};
+    }
+
+    std::generator<player_list> targeting_adjacent_players::possible_targets(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx) {
+        for (player_ptr first_target : origin->m_game->range_all_players(origin)) {
+            if (!check_player_filter(origin_card, origin, target_player.player_filter, first_target, ctx)
+                && !effect.get_error(origin_card, origin, first_target, ctx)
+            ) {
+                bool found = false;
+                for (player_ptr second_target : origin->m_game->range_all_players(origin)) {
+                    if (!check_second_target(origin_card, origin, first_target, second_target, ctx, max_distance)
+                        && !effect.get_error(origin_card, origin, second_target, ctx)
+                    ) {
+                        found = true;
+                        co_yield {first_target, second_target};
+                    }
+                }
+                if (!found) {
+                    co_yield {first_target};
+                }
+            }
+        }
+    }
+
+    game_string targeting_adjacent_players::get_error(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, const player_list &targets) {
+        if (targets.empty() || targets.size() > 2) {
+            return "ERROR_INVALID_TARGETS";
+        }
+        player_ptr first_target = targets[0];
+        MAYBE_RETURN(check_player_filter(origin_card, origin, target_player.player_filter, first_target, ctx));
+        if (targets.size() == 2) {
+            player_ptr second_target = targets[1];
+            MAYBE_RETURN(check_second_target(origin_card, origin, first_target, second_target, ctx, max_distance));
+        } else if (rn::any_of(origin->m_game->range_all_players(origin), [&](player_ptr second_target) {
+            return !check_second_target(origin_card, origin, first_target, second_target, ctx, max_distance);
+        })) {
             return "ERROR_TARGETS_NOT_ADJACENT";
         }
         for (player_ptr target : targets) {
@@ -24,19 +61,22 @@ namespace banggame {
         return {};
     }
 
-    prompt_string targeting_adjacent_players::on_prompt(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, player_pair targets) {
+    prompt_string targeting_adjacent_players::on_prompt(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, const player_list &targets) {
+        if (targets.size() == 1 && origin->m_game->num_alive() > 2) {
+            return {"PROMPT_SINGLE_TARGET", origin_card};
+        }
         return prompts::select_prompt(targets | rv::transform([&](player_ptr target) {
             return target_player.on_prompt(origin_card, origin, effect, ctx, target);
         }));
     }
 
-    void targeting_adjacent_players::add_context(card_ptr origin_card, player_ptr origin, const effect_holder &effect, effect_context &ctx, player_pair targets) {
+    void targeting_adjacent_players::add_context(card_ptr origin_card, player_ptr origin, const effect_holder &effect, effect_context &ctx, const player_list &targets) {
         for (player_ptr target : targets) {
             target_player.add_context(origin_card, origin, effect, ctx, target);
         }
     }
 
-    void targeting_adjacent_players::on_play(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, player_pair targets) {
+    void targeting_adjacent_players::on_play(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, const player_list &targets) {
         for (player_ptr target : targets) {
             effect.on_play(origin_card, origin, target, effect_flag::multi_target, ctx);
         }
--- src/target_types/valleyofshadows/adjacent_players.h
+++ src/target_types/valleyofshadows/adjacent_players.h
@@ -5,10 +5,8 @@
 
 namespace banggame {
 
-    using player_pair = std::array<player_ptr, 2>;
-
     struct targeting_adjacent_players {
-        using value_type = player_pair;
+        using value_type = player_list;
 
         targeting_player target_player;
         int max_distance;
@@ -25,22 +23,11 @@ namespace banggame {
             return args{ target_player.player_filter, max_distance };
         }
         
-        auto possible_targets(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx) {
-            auto all_players = origin->m_game->range_all_players(origin);
-            return rv::cartesian_product(all_players, all_players)
-                | rv::transform([](const auto &pair) {
-                    auto [target1, target2] = pair;
-                    return player_pair{target1, target2};
-                })
-                | rv::filter([=, &ctx, this](player_pair targets) {
-                    return !get_error(origin_card, origin, effect, ctx, targets);
-                });
-        }
-
-        game_string get_error(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, player_pair target);
-        prompt_string on_prompt(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, player_pair target);
-        void add_context(card_ptr origin_card, player_ptr origin, const effect_holder &effect, effect_context &ctx, player_pair target);
-        void on_play(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, player_pair target);
+        std::generator<player_list> possible_targets(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx);
+        game_string get_error(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, const player_list &target);
+        prompt_string on_prompt(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, const player_list &target);
+        void add_context(card_ptr origin_card, player_ptr origin, const effect_holder &effect, effect_context &ctx, const player_list &target);
+        void on_play(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx, const player_list &target);
     };
 
     DEFINE_TARGETING(adjacent_players, targeting_adjacent_players)
