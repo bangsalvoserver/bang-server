60a0b972fe51c64fae75cf01f71a4a0e6945f09e
Salvo Miosi <salvo.ilmiosi@gmail.com>
2026-03-16 19:11:37 +0100

    fix subtle bug in targeting_max_cards::is_possible

--- src/target_types/fistfulofcards/max_cards.cpp
+++ src/target_types/fistfulofcards/max_cards.cpp
@@ -3,7 +3,18 @@
 namespace banggame {
 
     bool targeting_max_cards::is_possible(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx) {
-        return contains_at_least(target_card.possible_targets(origin_card, origin, effect, ctx), 1);
+        if (confirmable) {
+            return contains_at_least(target_card.possible_targets(origin_card, origin, effect, ctx), 1);
+        } else {
+            auto targets = target_card.possible_targets(origin_card, origin, {}, ctx);
+            size_t required = 0, valid = 0;
+            for (card_ptr target : targets) {
+                if (required < ncards) ++required;
+                if (!target_card.get_error(origin_card, origin, effect, ctx, target)) ++valid;
+                if (required == ncards && valid >= required) return true;
+            }
+            return required > 0 && valid >= required;
+        }
     }
 
     card_list targeting_max_cards::random_target(card_ptr origin_card, player_ptr origin, const effect_holder &effect, const effect_context &ctx) {
@@ -22,7 +33,7 @@ namespace banggame {
             }
         } else {
             int count = 0;
-            for (card_ptr c : target_card.possible_targets(origin_card, origin, effect, ctx)) {
+            for (card_ptr c : target_card.possible_targets(origin_card, origin, {}, ctx)) {
                 ++count;
                 if (count >= ncards) break;
             }
