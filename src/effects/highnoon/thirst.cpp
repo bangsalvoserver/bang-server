d2ec231004fe688e3a8841429f433947f40ca361
Salvo Miosi <salvo.ilmiosi@gmail.com>
2026-08-06 21:48:31 +0200

    fix thirst: reduce number of cards drawn if >1

--- src/effects/highnoon/thirst.cpp
+++ src/effects/highnoon/thirst.cpp
@@ -8,7 +8,9 @@ namespace banggame {
 
     void equip_thirst::on_enable(card_ptr target_card, player_ptr target) {
         target->m_game->add_listener<event_type::init_request_draw>(target_card, [](player_ptr origin, shared_request_draw req) {
-            --req->num_cards_to_draw;
+            if (req->num_drawn_cards > 1) {
+                --req->num_cards_to_draw;
+            }
         });
     }
 }
