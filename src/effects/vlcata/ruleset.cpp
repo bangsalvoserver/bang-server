1f5423cd117e6c720ab4a4a1df31aa31238d7fe4
Sysr Vit (DES DOS IP AMS CM CS HADC) <Vit.Sysr@infineon.com>
2026-08-20 18:26:17 +0200

    fix of Vlcata

--- src/effects/vlcata/ruleset.cpp
+++ src/effects/vlcata/ruleset.cpp
@@ -1,12 +1,11 @@
 #include "ruleset.h"
 #include "game/expansion_set.h"
-#include "effects/dodgecity/ruleset.h"
 #include "game/game_table.h"
 
 namespace banggame {
 
     bool ruleset_vlcata::is_valid_with(const expansion_set &set) {
-        return set.contains(GET_RULESET(dodgecity));
+        return true;
     }
 
     void ruleset_vlcata::on_apply(game_ptr game) {
