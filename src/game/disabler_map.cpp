c305577acd90904c93df156ebec8ca73e4e996ca
Salvo Miosi <salvo.ilmiosi@gmail.com>
2026-09-14 22:29:56 +0200

    fix disable_use + get_disabler

--- src/game/disabler_map.cpp
+++ src/game/disabler_map.cpp
@@ -78,7 +78,7 @@ namespace banggame {
     }
 
     card_ptr disabler_map::get_disabler(const_card_ptr target_card, bool check_disable_use) const {
-        if (is_disableable_card(target_card)) {
+        if (check_disable_use || is_disableable_card(target_card)) {
             for (auto &[card_key, fun] : m_disablers) {
                 if ((!check_disable_use || fun.is_disable_use()) && fun(target_card)) {
                     return card_key.target_card;
